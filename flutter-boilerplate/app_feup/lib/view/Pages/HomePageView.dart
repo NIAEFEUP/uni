b4e8e78dc7f2f0a356b16d29e1cfd069cd87bdaf
Henrique Lima <henriquelim@hotmail.com>
2019-02-13 22:06:53 +0000

    Fixed height stretching to wrap content

--- flutter-boilerplate/app_feup/lib/view/Pages/HomePageView.dart
+++ flutter-boilerplate/app_feup/lib/view/Pages/HomePageView.dart
@@ -14,53 +14,21 @@ class HomePageView extends StatelessWidget {
   final String title;
 
 
-
   /*********** MAIN BUILD METHOD ***********/
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-      title: new Text(title),
-      ),
+        appBar: new AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text(title),
+        ),
 //      body: createCounterDisplay(context),
-        body: new GeneralCard( 
+        body: new GeneralCard(
           title: 'Horário',
-          child: Text('Meias e que sao do carago'),
+          child: Text("This\nis\na\nheight\ntest\nto\nknow\nif\nthe\ncard\nexpands")
         )
 //      floatingActionButton: createActionButton(context),
     );
   }
-
-
-
-//  Widget createActionButton(BuildContext context){
-//    return new FloatingActionButton(
-//      onPressed: () => onChanged(value+1),
-//      tooltip: 'Increment',
-//      child: new Icon(Icons.add),
-//    );
-//  }
-
-//  Widget createCounterDisplay(BuildContext context){
-//    return new Center(
-//      child: new Container(
-//        padding: EdgeInsets.all(16),
-//        child: new Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            new Text(
-//              'You have pushed the button this many times:',
-//              style: Theme.of(context).textTheme.body1,
-//            ),
-//            new Text(
-//              '$value',
-//              style: Theme.of(context).textTheme.title.apply(color: color),
-//            ),
-//          ],
-//        ),
-//      ) ,
-//    );
-//  }
 }
--- flutter-boilerplate/app_feup/lib/view/widgets/GeneralCardWidget.dart
+++ flutter-boilerplate/app_feup/lib/view/widgets/GeneralCardWidget.dart
@@ -11,43 +11,42 @@ class GeneralCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new ConstrainedBox(
-          constraints: new BoxConstraints(
-          minHeight: 150.0,
-      ),
-      child: new Card(
+    return new Card(
         margin: EdgeInsets.fromLTRB(12, 12, 12, 0),
         color: Color.fromARGB(0, 0, 0, 0),
         shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(this.borderRadius)),
+
         child: new Container(
-          height: 4*this.borderRadius,
-          decoration: BoxDecoration(
-              color: Theme.of(context).accentColor,
-              border: Border.all(color: Color.fromARGB(64, 0x46, 0x46, 0x46)),
-              borderRadius: BorderRadius.all(Radius.circular(this.borderRadius))),
-          child:
-          new Column(
-            children: <Widget>[
-              new Container(
-                child: Text(title,
-                    style: Theme.of(context).textTheme.title),
-                height: 26,
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-              ),
-              new Expanded(
-                child: new Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(this.borderRadius))),
-                  width: (double.infinity),
-                  padding: EdgeInsets.all(15.0),
-                  child: this.child,
+            decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                border: Border.all(width: 0.5, color: Color.fromARGB(64, 0x46, 0x46, 0x46)),
+                borderRadius: BorderRadius.all(Radius.circular(this.borderRadius))),
+            child:
+            new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Container(
+                  child: Text(title,
+                      style: Theme.of(context).textTheme.title),
+                  height: 26,
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                 ),
-              )
-            ],
-          ),
-        ),
+                new ConstrainedBox(
+                  constraints: new BoxConstraints(
+                    minHeight: 100.0,
+                  ),
+                  child: new Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(this.borderRadius))),
+                    width: (double.infinity),
+                    padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 8.0, bottom: 10.0),
+                    child: this.child,
+                  ),
+                ),
+              ],
+            ),
       ),
     );
 
