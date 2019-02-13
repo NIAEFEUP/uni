7be664a398f39405f74d7fed554106dcf3a8f80d
tiagocv <tcverdade@hotmail.com>
2019-02-13 16:49:48 +0000

    Begin creating cardview

--- flutter-boilerplate/app_feup/lib/view/Pages/HomePageView.dart
+++ flutter-boilerplate/app_feup/lib/view/Pages/HomePageView.dart
@@ -1,3 +1,4 @@
+import 'package:app_feup/view/Widgets/ScheduleWidget.dart';
 import 'package:flutter/material.dart';
 
 class HomePageView extends StatelessWidget {
@@ -21,38 +22,45 @@ class HomePageView extends StatelessWidget {
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(title),
+      title: new Text(title),
       ),
-      body: createCounterDisplay(context),
-      floatingActionButton: createActionButton(context),
+//      body: createCounterDisplay(context),
+        body: new ScheduleWidget( 
+          title: 'Horario',
+          widget: Text('ola'),
+        )
+//      floatingActionButton: createActionButton(context),
     );
   }
 
 
 
-  Widget createActionButton(BuildContext context){
-    return new FloatingActionButton(
-      onPressed: () => onChanged(value+1),
-      tooltip: 'Increment',
-      child: new Icon(Icons.add),
-    );
-  }
+//  Widget createActionButton(BuildContext context){
+//    return new FloatingActionButton(
+//      onPressed: () => onChanged(value+1),
+//      tooltip: 'Increment',
+//      child: new Icon(Icons.add),
+//    );
+//  }
 
-  Widget createCounterDisplay(BuildContext context){
-    return new Center(
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Text(
-            'You have pushed the button this many times:',
-            style: Theme.of(context).textTheme.body1,
-          ),
-          new Text(
-            '$value',
-            style: Theme.of(context).textTheme.title.apply(color: color),
-          ),
-        ],
-      ),
-    );
-  }
+//  Widget createCounterDisplay(BuildContext context){
+//    return new Center(
+//      child: new Container(
+//        padding: EdgeInsets.all(16),
+//        child: new Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            new Text(
+//              'You have pushed the button this many times:',
+//              style: Theme.of(context).textTheme.body1,
+//            ),
+//            new Text(
+//              '$value',
+//              style: Theme.of(context).textTheme.title.apply(color: color),
+//            ),
+//          ],
+//        ),
+//      ) ,
+//    );
+//  }
 }
