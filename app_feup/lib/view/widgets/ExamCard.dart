4c911bd970ceaff38ac54f03face33ea96d4507f
andrefmrocha <andrefmrocha@live.com.pt>
2019-02-16 19:22:47 +0000

    Added second exam, needs to figure out the issue with the borders to not be full width

--- app_feup/lib/view/widgets/ExamCard.dart
+++ app_feup/lib/view/widgets/ExamCard.dart
@@ -83,11 +83,83 @@ class ExamCard extends StatelessWidget{
                         ],
                       ),
                     ),
-                  )
+                  ),
+
+                ],
+              ),
+             )
+          ),
+          new Container(
+            child: Text(
+                (" " + this.exam.weekDay + ", " + this.exam.day + " de " + this.exam.month),
+                style: Theme.of(context).textTheme.subtitle),
+            alignment: Alignment.center,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Theme.of(context).accentColor,
+                  width: 1.0
+                ),
+                top: BorderSide(
+                  color: Theme.of(context).accentColor,
+                  width: 1.0
+                )
+              )
+            ),
+          ),
+          new Center(
+            child: new Container(
+              margin: EdgeInsets.only(top: 5.0),
+              child: new Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Column(
+                    children: <Widget>[
+                      new Container(
+                        child: Text(this.exam.begin,style: Theme.of(context).textTheme.body2),
+                        margin: EdgeInsets.only(top: 5.0),
+                      ),new Container(
+                        child: Text(this.exam.end,style: Theme.of(context).textTheme.body2),
+                        margin: EdgeInsets.only(top: 24.0),
+                      ),
+                    ],
+                  ),
+                  new ConstrainedBox(
+                    constraints: new BoxConstraints(
+                        minWidth: 210,
+                        minHeight: 30,
+                        maxWidth: 300,
+                        maxHeight: 65
+                    ),
+                    child: new Container(
+                      margin: EdgeInsets.only(left: 24.0, top: 2.0),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.all(Radius.circular(this.borderRadius)),
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          new Container(
+                            margin: EdgeInsets.only(top: 5.0),
+                            child: new Text(this.exam.subject, style: TextStyle(color: Theme.of(context).accentColor),),
+                          ),
+                          new Container(
+                            margin: EdgeInsets.only(top: 13.0),
+                            child: new Text(this.exam.rooms, style: TextStyle(color: Theme.of(context).accentColor),),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+
                 ],
               ),
              )
           )
+
         ],
       )
     );
