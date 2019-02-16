b187f3966a71e5f689c1ec94e1d08f4d47e8833a
Pedro Lima <pdrlima96@hotmail.com>
2019-02-16 17:50:55 +0000

    Merge with dev.

--- app_feup/lib/view/Pages/HomePageView.dart
+++ app_feup/lib/view/Pages/HomePageView.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/NavigationDrawer.dart';
+
 class HomePageView extends StatelessWidget {
   HomePageView({Key key}) : super(key: key);
 
@@ -7,6 +9,7 @@ class HomePageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("App FEUP")),
+      drawer: new NavigationDrawer(),
       body: createScrollableCardView(context),
       floatingActionButton: createActionButton(context),
     );
