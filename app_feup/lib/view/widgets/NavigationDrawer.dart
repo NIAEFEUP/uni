ef448e4184f59da7da88228e57080d2643a90609
Pedro Lima <pdrlima96@hotmail.com>
2019-02-19 11:42:51 +0000

    Added forgotten search input field.

--- app_feup/lib/view/widgets/NavigationDrawer.dart
+++ app_feup/lib/view/widgets/NavigationDrawer.dart
@@ -13,7 +13,6 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class NavigationDrawerState extends State<NavigationDrawer> {
-  int _selectedIndex = 0;
 
   final drawerItems = [
     "Área Pessoal",
@@ -30,43 +29,66 @@ class NavigationDrawerState extends State<NavigationDrawer> {
     StoreProvider.of<AppState>(context).dispatch(updateSelectedPage(drawerItems[index]));
 
     Navigator.of(context).pop(); // close the drawer
-//    Navigator.push(
-//      context,
-//      new MaterialPageRoute(
-//        builder: (context) => _getDrawerItemScreen(_selectedIndex),
-//      ),
-//    );
   }
 
   _buildBorder(name) {
     return (name == StoreProvider.of<AppState>(context).state.content["selected_page"])?  (const BoxDecoration( border: Border( bottom: BorderSide(width: 5.0, color: primaryColor)))) : null;
   }
 
+  Widget createSearchInputField()
+  {
+    return new ListTile(
+        title:
+        new Padding (
+            padding: const EdgeInsets.only(bottom: 20.0,),
+            child: new Row (
+                children: <Widget> [
+                  new Flexible (
+                    child: new TextField (
+                        decoration: new InputDecoration(
+                            hintText: "Pesquisa..."
+                        ),
+                    ),
+                  ),
+                ]
+            )
+        )
+    );
+  }
+
+  Widget createDrawerNavigationOption(String d, int i) {
+    return new ListTile(
+      title:
+      new Row(
+        children: <Widget>[
+          new Container(
+            decoration: _buildBorder(d),
+            child: new Text(d, style: TextStyle(fontSize: 24.0, color: primaryColor)),
+          ),
+        ],
+      ),
+      selected: d == StoreProvider.of<AppState>(context).state.content["selected_page"],
+      onTap: () => _onSelectItem(i),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     List<Widget> drawerOptions = [];
-    for (var i = 0; i < drawerItems.length; i++) {
-      var d = drawerItems[i];
 
-      drawerOptions.add(new ListTile(
-        title:
-            new Row(
-              children: <Widget>[
-                new Container(
-                  decoration: _buildBorder(d),
-                  child: new Text(d, style: TextStyle(fontSize: 24.0, color: primaryColor)),
-                ),
-              ],
-            ),
-        selected: d == StoreProvider.of<AppState>(context).state.content["selected_page"],
-        onTap: () => _onSelectItem(i),
-      ));
+    drawerOptions.add(createSearchInputField());
+
+    for (var i = 0; i < drawerItems.length; i++) {
+      drawerOptions.add(createDrawerNavigationOption(drawerItems[i], i));
     }
 
     return new Drawer(
-        child: new ListView(
-          children: drawerOptions,
-        ),
+        child: new Padding (
+          padding: EdgeInsets.all(20.0),
+          child: new ListView(
+            children: drawerOptions,
+          ),
+        )
     );
   }
 }
