2ed47208a3005937a364890f22f7e4b443c3e185
Nuno Lopes <up201605337@fe.up.pt>
2019-02-21 19:07:03 +0000

    Added connection between Home Page and Profile Page

--- app_feup/lib/view/Pages/HomePageView.dart
+++ app_feup/lib/view/Pages/HomePageView.dart
@@ -2,10 +2,7 @@ import 'package:app_feup/controller/homePage.dart';
 import 'package:flutter/material.dart';
 import '../widgets/GenericCard.dart';
 import '../widgets/ExamCard.dart';
-
 import 'ProfilePageView.dart';
-
-
 import '../widgets/NavigationDrawer.dart';
 
 class HomePageView extends StatelessWidget {
@@ -16,7 +13,25 @@ class HomePageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("App FEUP")),
+      appBar: new AppBar(
+        title: new Text("Área Pessoal", textAlign: TextAlign.start),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => {Navigator.pushReplacement(context,new MaterialPageRoute(builder: (__) => new ProfilePageView()))},
+            child: Container(
+                width: 45.0,
+                height: 45.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      "https://dei.fe.up.pt/gig/wp-content/uploads/sites/4/2017/02/AAS_Jorn-1.jpg")
+                  )
+                )
+              ),
+          ),
+        ],),
       drawer: new NavigationDrawer(),
       body: createScrollableCardView(context),
       floatingActionButton: createActionButton(context),
@@ -25,7 +40,7 @@ class HomePageView extends StatelessWidget {
 
   Widget createActionButton(BuildContext context){
     return new FloatingActionButton(
-      onPressed: () => {Navigator.pushReplacement(context,new MaterialPageRoute(builder: (__) => new ProfilePageView()))}, //Add FAB functionality here
+      onPressed: () => {}, //Add FAB functionality here
       tooltip: 'Add widget',
       child: new Icon(Icons.add),
     );
