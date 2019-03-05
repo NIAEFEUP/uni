2410bfeabdab9353a93d40cd4d59c4342490e094
Andre Rocha <andrefmrocha@live.com.pt>
2019-03-05 16:54:24 +0000

    Added Binding Observers to refresh state on resume

--- app_feup/lib/view/Pages/GeneralPageView.dart
+++ app_feup/lib/view/Pages/GeneralPageView.dart
@@ -4,9 +4,25 @@ import 'package:app_feup/model/AppState.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:app_feup/controller/loadinfo.dart';
+import 'package:app_feup/controller/LifecycleEventHandler.dart';
 
 
-abstract class GeneralPageView extends StatelessWidget {
+class GeneralPage extends StatefulWidget{
+  GeneralPage({
+    Key key
+  }): super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return GeneralPageView();
+  }
+
+
+}
+
+class GeneralPageView extends State<GeneralPage> {
+
+  WidgetsBindingObserver lifeCycleEventHandler;
 
   @override
   Widget build(BuildContext context) {
@@ -63,5 +79,26 @@ abstract class GeneralPageView extends StatelessWidget {
         );
       },
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    StoreConnector<AppState, VoidCallback>(
+      converter: (store){
+
+        return () =>
+          WidgetsBinding.instance.addObserver(this.lifeCycleEventHandler =  new LifecycleEventHandler(store: store));
+      },
+      builder: (context, observer){
+        observer;
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this.lifeCycleEventHandler);
+    super.dispose();
   }
 }
