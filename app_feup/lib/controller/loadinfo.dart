e0ba1bb24dbd3d0a2e195ff5c71790d94d733f38
Pedro Lima <pdrlima96@hotmail.com>
2019-04-03 22:13:48 +0100

    Loading of lectures fixed.

--- app_feup/lib/controller/loadinfo.dart
+++ app_feup/lib/controller/loadinfo.dart
@@ -2,7 +2,7 @@ import 'package:app_feup/redux/actionCreators.dart';
 
 loadUserInfoToState(store){
   store.dispatch(updateStateBasedOnLocalUserExams());
-  store.dispatch(updateStateBasedOnLocalUserExams());
+  store.dispatch(updateStateBasedOnLocalUserLectures());
   store.dispatch(getUserExams());
   store.dispatch(getUserSchedule());
 }
