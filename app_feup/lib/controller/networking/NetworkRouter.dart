c24e89d2265bb2a044148cdb92d7e34f84012533
Andre Rocha <andrefmrocha@live.com.pt>
2019-09-16 17:42:28 +0100

    Removed prints

--- app_feup/lib/controller/networking/NetworkRouter.dart
+++ app_feup/lib/controller/networking/NetworkRouter.dart
@@ -82,9 +82,6 @@ class NetworkRouter {
     final Map<String, String> headers = Map<String, String>();
     headers['cookie'] = cookies;
 
-
-    print(url);
-    print(cookies);
     return http.get(url, headers: headers);
   }
 
