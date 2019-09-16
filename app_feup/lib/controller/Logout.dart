87b97c3d0d82b8f1091421ecbb0850a43288f127
reeckset <henriquemlima7@gmail.com>
2019-09-16 11:48:25 +0100

    Small adjustments

--- app_feup/lib/controller/Logout.dart
+++ app_feup/lib/controller/Logout.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:app_feup/controller/local_storage/AppDatabase.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:app_feup/model/AppState.dart';
