51267c9c3985b4458f5daf2fd07f29c3f981d664
HenriJSantos <henrijsantos99@gmail.com>
2020-01-24 16:50:26 +0000

    Adding stop pages model file

---
+++ app_feup/lib/model/StopsPageModel.dart
@@ -0,0 +1,17 @@
+import 'package:app_feup/view/Pages/BusStopNextArrivalsPage.dart';
+import 'package:flutter/material.dart';
+
+class StopsPage extends StatefulWidget{
+
+  const StopsPage({Key key}) : super(key: key);
+
+  @override
+  _StopsPageState createState() => new _StopsPageState();
+}
+
+class _StopsPageState extends State<StopsPage> with SingleTickerProviderStateMixin{
+  @override
+  Widget build(BuildContext context) {
+    return new BusStopNextArrivalsPage();
+  }
+}
