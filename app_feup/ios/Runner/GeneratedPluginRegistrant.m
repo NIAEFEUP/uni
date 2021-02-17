8c682aaea510a7edee3e160b0714fcb857e8156e
Bruno Mendes <brod56@sapo.pt>
2021-02-17 17:43:25 +0000

    Add url expression detector

--- app_feup/ios/Runner/GeneratedPluginRegistrant.m
+++ app_feup/ios/Runner/GeneratedPluginRegistrant.m
@@ -28,6 +28,12 @@
 @import sqflite;
 #endif
 
+#if __has_include(<url_launcher/UrlLauncherPlugin.h>)
+#import <url_launcher/UrlLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
@@ -35,6 +41,7 @@
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
   [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
   [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
 }
 
 @end
