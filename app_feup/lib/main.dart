3783072c513003d41ac34685ebb10a0f421711b6
Bruno Mendes <brod56@sapo.pt>
2021-02-17 17:43:25 +0000

    Add url expression detector

--- app_feup/lib/main.dart
+++ app_feup/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
--- app_feup/lib/view/Widgets/terms_and_conditions.dart
+++ app_feup/lib/view/Widgets/terms_and_conditions.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:uni/controller/load_static/terms_and_conditions.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/gestures.dart';
 
 class TermsAndConditions extends StatelessWidget {
   static String termsAndConditionsSaved = 'Carregando os Termos e Condições...';
@@ -14,7 +17,29 @@ class TermsAndConditions extends StatelessWidget {
           if (termsAndConditions.connectionState == ConnectionState.done) {
             termsAndConditionsSaved = termsAndConditions.data;
           }
-          return Text(termsAndConditionsSaved);
+          return RichText(
+              text: TextSpan(children: linkify(termsAndConditionsSaved)));
         });
+  }
+
+  List<TextSpan> linkify(String txt) {
+    final RegExp exp = RegExp(
+        r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?');
+    List<TextSpan> lst = List<TextSpan>();
+    for (var s in txt.split(' ')) {
+      if (exp.hasMatch(s)) {
+        lst.add(TextSpan(
+            text: s + ' ',
+            style: TextStyle(color: Colors.blue, fontSize: 20.0),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                launch(exp.firstMatch(s).group(0));
+                print("hey!");
+              }));
+      } else {
+        lst.add(TextSpan(text: s + ' ', style: TextStyle(color: Colors.black)));
+      }
+    }
+    return lst;
   }
 }
