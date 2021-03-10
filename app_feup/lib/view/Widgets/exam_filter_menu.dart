de5d2f6148e1ad3a20fbce048a99bd5e46d0f787
tiago-falves <tiago.falves98@gmail.com>
2021-03-10 23:04:08 +0000

    StoreConncetor making checkbox not update

--- app_feup/lib/view/Widgets/exam_filter_menu.dart
+++ app_feup/lib/view/Widgets/exam_filter_menu.dart
@@ -7,10 +7,14 @@ import 'package:uni/redux/action_creators.dart';
 
 // ignore: must_be_immutable
 class ExamFilterMenu extends StatefulWidget {
-  final Map<String, bool> checkboxes;
-
-  ExamFilterMenu(this.checkboxes);
-
+  //TODO
+  // Isto com as checkboxes dava, mas ao adicionar o storeConnector isto deixou de atualizar
+  //Verificar que os exams da epoca de covid aparecem com ?
+  //Verificar o que acontece quando ele nao tem shared preferences
+  //StoreConnector mais dentro tb nao funciona
+  //uma opçao é ir buscar os dados aqui, e no state usar o valor
+  //Versao a funcionar no commit anterior
+  //Ver a questão dos exams covid
   @override
   _ExamFilterMenuState createState() => _ExamFilterMenuState();
 }
@@ -18,35 +22,40 @@ class ExamFilterMenu extends StatefulWidget {
 class _ExamFilterMenuState extends State<ExamFilterMenu> {
   showAlertDialog(BuildContext context) {
     // set up the AlertDialog
-    final AlertDialog alert = AlertDialog(
-      content: Container(
-        height: 300.0,
-        width: 200.0,
-        child: StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-          return ListView(
-            children: widget.checkboxes.keys.map((String key) {
-              return CheckboxListTile(
-                title: Text(key),
-                value: widget.checkboxes[key],
-                onChanged: (bool value) {
-                  setState(() {
-                    widget.checkboxes[key] = value;
-                  });
-                  StoreProvider.of<AppState>(context)
-                      .dispatch(setFilteredExams(key, Completer()));
-                },
-              );
-            }).toList(),
+    final store = StoreConnector<AppState, Map<String, bool>>(
+        converter: (store) => store.state.content['filteredExams'],
+        builder: (context, filteredExams) {
+          return AlertDialog(
+            content: Container(
+                height: 300.0,
+                width: 200.0,
+                child: StatefulBuilder(
+                    builder: (BuildContext context, StateSetter setState) {
+                  return ListView(
+                    children: filteredExams.keys.map((String key) {
+                      return CheckboxListTile(
+                        title: Text(key),
+                        value: filteredExams[key],
+                        onChanged: (bool value) {
+                          setState(() {
+                            filteredExams[key] = value;
+                            //Not calling OnChanged?
+                            print("ZAAAS" + value.toString());
+                            StoreProvider.of<AppState>(context)
+                                .dispatch(setFilteredExams(key, Completer()));
+                          });
+                        },
+                      );
+                    }).toList(),
+                  );
+                })),
           );
-        }),
-      ),
-    );
+        });
 
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return alert;
+        return store;
       },
     );
   }
