2173a5dbe952dc03da3e2667690cf1204e9e927f
Bruno Mendes <brod56@sapo.pt>
2021-04-06 10:51:27 +0100

    Remove unused imports

--- app_feup/lib/view/Widgets/terms_and_conditions.dart
+++ app_feup/lib/view/Widgets/terms_and_conditions.dart
@@ -4,8 +4,6 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:uni/controller/load_static/terms_and_conditions.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import 'package:sentry/sentry.dart';
-
 class TermsAndConditions extends StatelessWidget {
   static String termsAndConditionsSaved = 'Carregando os Termos e Condições...';
 
