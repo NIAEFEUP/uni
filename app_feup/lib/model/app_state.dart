gui1612/project-schrodinger

Filter is working

app_feup/lib/model/app_state.dart
@@ -16,7 +16,7 @@ class AppState {
     return {
       'schedule': List<Lecture>(),
       'exams': List<Exam>(),
-      'filteredExams': Map<String, bool>(), //TODO Alterar por uma classe
+      'filteredExams': Map<String, bool>(),
       'scheduleStatus': RequestStatus.none,
       'loginStatus': RequestStatus.none,
       'examsStatus': RequestStatus.none,

NIAEFEUP/uni

app_feup/lib/model/app_state.dart
@@ -16,7 +16,7 @@ class AppState {
     return {
       'schedule': <Lecture>[],
       'exams': <Exam>[],
-      'filteredExam': Map<String, bool>(), //TODO Isto devia estar aqui sequer?
+      'filteredExam': Map<String, bool>(),
       'scheduleStatus': RequestStatus.none,
       'loginStatus': RequestStatus.none,
       'examsStatus': RequestStatus.none,