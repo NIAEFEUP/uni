e738395f6f44df041d286e48212db5c90ac8ac6f
Bruno Mendes <brod56@sapo.pt>
2021-12-23 11:19:41 +0000

    Fix namespace issue

--- app_feup/lib/controller/local_storage/app_shared_preferences.dart
+++ app_feup/lib/controller/local_storage/app_shared_preferences.dart
@@ -72,15 +72,10 @@ class AppSharedPreferences {
     return ThemeMode.values[prefs.getInt(themeMode) ?? ThemeMode.system.index];
   }
 
+  /// Set new app theme mode.
   static Future<bool> setThemeMode(ThemeMode thmMode) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.setInt(themeMode, thmMode.index);
-  }
-
-  static Future<bool> setNextThemeMode() async {
-    final prefs = await SharedPreferences.getInstance();
-    final themeIndex = (await getThemeMode()).index;
-    return prefs.setInt(themeMode, (themeIndex + 1) % 3);
   }
 
   /// Deletes the user's student number and passoword.
