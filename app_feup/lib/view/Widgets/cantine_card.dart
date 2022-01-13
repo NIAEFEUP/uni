b306a7b46250b5be0c9ae3dff9ea782004718336
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2022-01-13 15:46:55 +0000

    Specify which meal is displayed

--- app_feup/lib/view/Widgets/cantine_card.dart
+++ app_feup/lib/view/Widgets/cantine_card.dart
@@ -3,7 +3,6 @@ import 'package:uni/model/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:uni/utils/constants.dart' as Constants;
 import 'package:uni/view/Widgets/cantine_row.dart';
 import 'package:uni/view/Widgets/request_dependent_widget_builder.dart';
 import 'package:uni/view/Widgets/row_container.dart';
@@ -13,8 +12,9 @@ import 'generic_card.dart';
 class CantineCard extends GenericCard {
   CantineCard({Key key}) : super(key: key);
 
-  CantineCard.fromEditingInformation(Key key, bool editingMode, Function onDelete)
-      : super.fromEditingInformation(key, editingMode, onDelete);
+  CantineCard.fromEditingInformation(
+    Key key, bool editingMode, Function onDelete) 
+  : super.fromEditingInformation(key, editingMode, onDelete);
 
   @override
   String getTitle() => 'Cantinas';
@@ -49,22 +49,24 @@ class CantineCard extends GenericCard {
     );
   }
 
-  List<Widget> getCantineRows(context, exams) {
+  List<Widget> getCantineRows(context, cantines) {
     final List<Widget> rows = <Widget>[];
-    rows.add(this.createRowFromCantine(context, "Cantina da FEUP"));
+    rows.add(this.createRowFromCantine(context, 'Cantina da FEUP - Almoço'));
 
     return rows;
   }
 
   Widget createRowFromCantine(context, String cantine) { // TODO: Cantine class
     return Column(children: [
-      DateRectangle(date: "Segunda" + ', ' + "08" + ' de ' + "Janeiro"), // cantine.nextSchoolDay
+      DateRectangle(date: 'Segunda' + ', ' + '08' + ' de ' + 'Janeiro'),
+       // cantine.nextSchoolDay
       Container(
         child: Center(
           child: Container(
               padding: EdgeInsets.all(12.0),
               decoration: BoxDecoration(
-              border: Border(bottom: BorderSide(width: 0.1, color: Theme.of(context).accentColor))),
+              border: Border(bottom: BorderSide(
+                width: 0.1, color: Theme.of(context).accentColor))),
               child: Text(cantine)
           )
         ),
@@ -73,10 +75,10 @@ class CantineCard extends GenericCard {
         child: RowContainer(
           color: Theme.of(context).backgroundColor,
           child:  CantineRow(
-            local: "Cantina Feup",
-            meatMenu: "Tripas à moda do Porto",
-            fishMenu: "Bacalhau à Brás",
-            vegetarianMenu: "Risoto de shitake",
+            local: cantine,
+            meatMenu: 'Tripas à moda do Porto',
+            fishMenu: 'Bacalhau à Brás',
+            vegetarianMenu: 'Risoto de shitake',
          )
         ),
       ),
