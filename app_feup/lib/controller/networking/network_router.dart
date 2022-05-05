61b893284037ae872b7aa316ca9c19801164b92c
thePeras <jtopereira@gmail.com>
2022-05-05 14:33:21 +0100

    dialog

--- app_feup/lib/controller/networking/network_router.dart
+++ app_feup/lib/controller/networking/network_router.dart
@@ -232,24 +232,25 @@ class NetworkRouter {
   }
 
   // Generates MB reference to add money to print balance
-  static Future<http.Response> generatePrintMoneyReference(
-      double amount, Session session) async {
-    if (amount < 1) return Future.error('Amount less than 1,00€');
+  static Future generatePrintMoneyReference(double amount) async {
+      if (amount < 1) return Future.error('Amount less than 1,00€');
+      final Session session = store.state.content['session'];
+      final url = NetworkRouter.getBaseUrlFromSession(session) +
+          'gpag_ccorrentes_geral.gerar_mb';
 
-    final url = NetworkRouter.getBaseUrlFromSession(session) +
-        'gpag_ccorrentes_geral.gerar_mb';
+      final Map data = {
+        'p_tipo_id': '3',
+        'pct_codigo': session.studentNumber,
+        'p_valor': '1',
+        'p_valor_livre': amount.toStringAsFixed(2).trim().replaceAll('.', ',')
+      };
 
-    final Map data = {
-      'p_tipo_id': '3',
-      'pct_codigo': session.studentNumber,
-      'p_valor': '1',
-      'p_valor_livre': amount.toStringAsFixed(2).trim().replaceAll('.', ',')
-    };
+      final Map<String, String> headers = Map<String, String>();
+      headers['cookie'] = session.cookies;
+      headers['content-type'] = 'application/x-www-form-urlencoded';
 
-    final Map<String, String> headers = Map<String, String>();
-    headers['cookie'] = session.cookies;
-    headers['content-type'] = 'application/x-www-form-urlencoded';
-
-    return await http.post(url.toUri(), headers: headers, body: data);
+      final response =
+          await http.post(url.toUri(), headers: headers, body: data);
+      return response;
   }
 }
--- app_feup/lib/view/Widgets/create_print_mb_dialog.dart
+++ app_feup/lib/view/Widgets/create_print_mb_dialog.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:uni/controller/networking/network_router.dart';
 
-const IconData arrow_drop_down_rounded =
-    IconData(0xf577, fontFamily: 'MaterialIcons');
-const IconData arrow_drop_up_rounded =
-    IconData(0xf578, fontFamily: 'MaterialIcons');
-    
 Future<void> addMoneyDialog(BuildContext context) async {
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    final TextEditingController _controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _controller =
+      TextEditingController(text: '1,00 €');
 
-    final CurrencyTextInputFormatter formatter = CurrencyTextInputFormatter(
+  final CurrencyTextInputFormatter formatter = CurrencyTextInputFormatter(
       locale: 'pt-PT', decimalDigits: 2, symbol: '€ ');
 
-    double valueTextToNumber(String value) =>
+  double valueTextToNumber(String value) =>
       double.parse(value.substring(0, value.length - 2).replaceAll(',', '.'));
-    String numberToValueText(double number) => 
+  String numberToValueText(double number) =>
       formatter.format(number.toStringAsFixed(2));
-    
-    return showDialog(
+
+  generateReference(context, amount) async {
+    final response = await NetworkRouter.generatePrintMoneyReference(amount);
+    Navigator.of(context).pop(false);
+  }
+
+  return showDialog(
       context: context,
       builder: (BuildContext context) {
         //State
         double value = 1.00;
-        double iva = value * 0.23;
+        double iva = value * 0.19;
         double total = value - iva;
 
         return StatefulBuilder(builder: (context, setState) {
@@ -46,61 +48,59 @@ Future<void> addMoneyDialog(BuildContext context) async {
                   children: [
                     Row(children: [
                       IconButton(
-                        icon:
-                            const Icon(Icons.indeterminate_check_box),
+                        icon: Icon(Icons.indeterminate_check_box),
                         tooltip: 'Decrementar 1,00€',
                         onPressed: () {
                           final decreasedValue =
-                            valueTextToNumber(_controller.text) - 1;
+                              valueTextToNumber(_controller.text) - 1;
                           if (decreasedValue < 1) return;
 
-                        _controller.value = TextEditingValue(
-                          text: numberToValueText(decreasedValue));
+                          _controller.value = TextEditingValue(
+                              text: numberToValueText(decreasedValue));
                         },
                       ),
-                      Expanded(child: 
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 5, vertical: 10),
+                      Expanded(
+                        child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 5, vertical: 10),
                           child: TextFormField(
-                            controller: _controller,
-                            inputFormatters: [formatter],
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.right,
-                            decoration: InputDecoration(
-                              suffixText: '€',
-                              border: OutlineInputBorder(),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                  BorderSide(color: Colors.red))),
-                            onTap: () {
-                              _controller.value = TextEditingValue(
-                                text: '',
-                                selection:
-                                  TextSelection.collapsed(offset: 0));
-                            },
-                            onChanged: (string) {
-                              _controller.value = TextEditingValue(
-                                text: string,
-                                selection: TextSelection.collapsed(
-                                  offset: string.length));
-                            }
-                          ),
+                              controller: _controller,
+                              inputFormatters: [formatter],
+                              keyboardType: TextInputType.number,
+                              textAlign: TextAlign.right,
+                              decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color:
+                                              Theme.of(context).accentColor))),
+                              onTap: () {
+                                _controller.value = TextEditingValue(
+                                    text: '',
+                                    selection:
+                                        TextSelection.collapsed(offset: 0));
+                              },
+                              onChanged: (string) {
+                                _controller.value = TextEditingValue(
+                                    text: string,
+                                    selection: TextSelection.collapsed(
+                                        offset: string.length));
+                              }),
                         ),
                       ),
                       IconButton(
-                            icon: const Icon(Icons.add_box),
-                            tooltip: 'Incrementar 1,00€',
-                            onPressed: () {
-                              _controller.value = TextEditingValue(
-                                text: numberToValueText(
-                                  valueTextToNumber(_controller.text) + 1)
-                              );
-                            },
-                          )
-                        ]
-                      ),
-                      Row(
+                        icon: const Icon(Icons.add_box),
+                        tooltip: 'Incrementar 1,00€',
+                        onPressed: () {
+                          _controller.value = TextEditingValue(
+                              text: numberToValueText(
+                                  valueTextToNumber(_controller.text) + 1));
+                        },
+                      )
+                    ]),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, bottom: 10),
+                      child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text('IVA 23%:',
@@ -110,26 +110,27 @@ Future<void> addMoneyDialog(BuildContext context) async {
                               style: Theme.of(context).textTheme.headline3),
                         ],
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Crédito a adicionar: ',
-                              textAlign: TextAlign.start,
-                              style: Theme.of(context).textTheme.headline2),
-                          Text(numberToValueText(total),
-                              style: Theme.of(context).textTheme.headline3),
-                        ]
-                      ),
-                      ElevatedButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        child: Text('Gerar Refêrencia'),
-                      )
-                    ],
-                  )
-                ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 20),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Crédito a adicionar: ',
+                                textAlign: TextAlign.start,
+                                style: Theme.of(context).textTheme.headline2),
+                            Text(numberToValueText(total),
+                                style: Theme.of(context).textTheme.headline3),
+                          ]),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => generateReference(context, value),
+                      child: Text('Gerar Refêrencia'),
+                    )
+                  ],
+                )),
             title: Text('Adicionar quota'),
           );
         });
-      }
-    );
-  }
+      });
+}
