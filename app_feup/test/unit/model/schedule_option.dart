7fb635f1e9f1922c610000a21615177bc18e1e07
biromiro <n.r.t.c@hotmail.com>
2022-05-22 12:28:47 +0100

    Fix test errors

--- app_feup/test/unit/model/schedule_option.dart
+++ app_feup/test/unit/model/schedule_option.dart
@@ -23,198 +23,329 @@ class LectureMock extends Mock implements Lecture {
 
 void main() {
   group('ScheduleOption', () {
-    test('New instance has correct name and no classes selected', () {
-      final ScheduleOption instance = ScheduleOption.newInstance();
-      for (int i = 0; i < 7; i++) {
-        expect(instance.getLectures(i).length, 0);
-      }
-
-      expect(instance.name, 'Novo Horário');
-    });
-
     test('Get lectures', () {
-      final Map<CourseUnit, CourseUnitClass> classes = Map();
-      final CourseUnit courseUnit1 = CourseUnit();
-      final CourseUnit courseUnit2 = CourseUnit();
-
-      classes[courseUnit1] = CourseUnitClass(lectures: [
+      final Map<String, String> classes = Map();
+      final CourseUnitClass courseUnitClass1 = CourseUnitClass(
+        name: '3LEIC01',
+          lectures: [
         LectureMock(0, '10h00', 'A'),
         LectureMock(1, '09h30', 'B'),
       ]);
-      classes[courseUnit2] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass2 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(1, '08h30', 'C'),
       ]);
-      final ScheduleOption instance = ScheduleOption(classesSelected: classes);
 
-      expect(instance.getLectures(0).length, 1);
-      final List<Lecture> day1Lectures = instance.getLectures(1);
+      final CourseUnit courseUnit1 = CourseUnit(
+          abbreviation: 'ABC',
+          classes: [courseUnitClass1]
+      );
+      final CourseUnit courseUnit2 = CourseUnit(
+          abbreviation: 'BAC',
+        classes: [courseUnitClass2]
+      );
+
+      classes[courseUnit1.abbreviation] = courseUnitClass1.name;
+      classes[courseUnit2.abbreviation] = courseUnitClass2.name;
+
+      final ScheduleOption instance = ScheduleOption(
+          id: 1,
+          name: 'Horário Teste',
+          classesSelected: classes,
+          preference: 0,
+      );
+
+      final List<CourseUnit> courseUnits = [courseUnit1, courseUnit2];
+      expect(instance.getLectures(0, courseUnits).length, 1);
+      final List<Lecture> day1Lectures = instance.getLectures(1, courseUnits);
       expect(day1Lectures.length, 2);
       // Check order of lectures
       expect(day1Lectures[0].startTime, '08h30');
       expect(day1Lectures[1].startTime, '09h30');
       for (int i = 2; i < 7; i++) {
-        expect(instance.getLectures(i).length, 0);
+        expect(instance.getLectures(i, courseUnits).length, 0);
       }
     });
 
     test('Change class', () {
-      final Map<CourseUnit, CourseUnitClass> classes = Map();
-      final CourseUnit courseUnit1 = CourseUnit();
+      final Map<String, String> classes = Map();
+      final CourseUnitClass courseUnitClass1 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
+        LectureMock(0, '10h00', 'A'),
+        LectureMock(1, '09h30', 'B'),
+      ]);
+      final CourseUnitClass courseUnitClass2 = CourseUnitClass(
+          name: '3LEIC02',
+          lectures: [
+        LectureMock(1, '08h30', 'C'),
+      ]);
 
-      classes[courseUnit1] = CourseUnitClass(lectures: [
-        LectureMock(0, '08h30', 'A'),
-        LectureMock(1, '08h30', 'B'),
-      ]);
-      classes[courseUnit1] = CourseUnitClass(lectures: [
-        LectureMock(1, '08h30', 'B'),
-      ]);
+      final CourseUnit courseUnit1 = CourseUnit(
+          abbreviation: 'ABC',
+        classes: [courseUnitClass1, courseUnitClass2]
+      );
+
+      classes[courseUnit1.abbreviation] = courseUnitClass1.name;
+      classes[courseUnit1.abbreviation] = courseUnitClass2.name;
+
       final ScheduleOption instance = ScheduleOption(classesSelected: classes);
-
+      final List<CourseUnit> courseUnits = [courseUnit1];
       for (int i = 0; i < 7; i++) {
         if (i != 1) {
-          expect(instance.getLectures(i).length, 0);
+          expect(instance.getLectures(i, courseUnits).length, 0);
         } else {
-          expect(instance.getLectures(i).length, 1);
+          expect(instance.getLectures(i, courseUnits).length, 1);
         }
       }
     });
 
     test('No collisions', () {
-      final Map<CourseUnit, CourseUnitClass> classes = Map();
-      final CourseUnit courseUnit1 = CourseUnit();
-      final CourseUnit courseUnit2 = CourseUnit();
-      final CourseUnit courseUnit3 = CourseUnit();
-
-      classes[courseUnit1] = CourseUnitClass(lectures: [
+      final Map<String, String> classes = Map();
+      final CourseUnitClass courseUnitClass1 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(0, '13h30', 'A'),
         LectureMock(1, '14h30', 'B'),
         LectureMock(1, '15h30', 'C'),
         LectureMock(3, '16h30', 'B'),
       ]);
-      classes[courseUnit2] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass2 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(5, '13h30', 'A'),
         LectureMock(5, '14h30', 'B'),
         LectureMock(6, '15h30', 'A'),
         LectureMock(4, '16h30', 'A'),
       ]);
-      classes[courseUnit3] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass3 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(2, '13h30', 'B'),
         LectureMock(3, '14h30', 'C'),
         LectureMock(5, '15h30', 'C'),
         LectureMock(3, '16h30', 'A'),
       ]);
+
+      final CourseUnit courseUnit1 = CourseUnit(
+          abbreviation: 'ABC',
+        classes: [courseUnitClass1]
+      );
+      final CourseUnit courseUnit2 = CourseUnit(
+          abbreviation: 'BAC',
+          classes: [courseUnitClass2]
+      );
+      final CourseUnit courseUnit3 = CourseUnit(
+          abbreviation: 'CAB',
+          classes: [courseUnitClass3]
+      );
+
+      classes[courseUnit1.abbreviation] = courseUnitClass1.name;
+      classes[courseUnit2.abbreviation] = courseUnitClass2.name;
+      classes[courseUnit3.abbreviation] = courseUnitClass3.name;
+
       final ScheduleOption instance = ScheduleOption(classesSelected: classes);
+      final List<CourseUnit> courseUnits = [
+        courseUnit1,
+        courseUnit2,
+        courseUnit3
+      ];
 
       for (int i = 0; i < 7; i++) {
-        expect(instance.hasCollisions(i), false);
+        expect(instance.hasCollisions(i, courseUnits), false);
       }
     });
 
     test('One collision', () {
-      final Map<CourseUnit, CourseUnitClass> classes = Map();
-      final CourseUnit courseUnit1 = CourseUnit();
-      final CourseUnit courseUnit2 = CourseUnit();
-      final CourseUnit courseUnit3 = CourseUnit();
-
-      classes[courseUnit1] = CourseUnitClass(lectures: [
+      final Map<String, String> classes = Map();
+      final CourseUnitClass courseUnitClass1 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(0, '13h30', 'A'),
         LectureMock(1, '14h30', 'B'),
         LectureMock(1, '15h30', 'C'),
         LectureMock(3, '16h30', 'B'),
       ]);
-      classes[courseUnit2] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass2 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(5, '13h30', 'A'),
         LectureMock(5, '14h30', 'B'),
         LectureMock(6, '15h30', 'A'),
         LectureMock(4, '16h30', 'A'),
       ]);
-      classes[courseUnit3] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass3 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(2, '13h30', 'B'),
         LectureMock(3, '14h30', 'C'),
         LectureMock(5, '15h30', 'A'),
         LectureMock(3, '16h30', 'A'),
       ]);
+
+      final CourseUnit courseUnit1 = CourseUnit(
+          abbreviation: 'ABC',
+        classes: [courseUnitClass1]
+      );
+      final CourseUnit courseUnit2 = CourseUnit(
+          abbreviation: 'BAC',
+          classes: [courseUnitClass2]
+      );
+      final CourseUnit courseUnit3 = CourseUnit(
+          abbreviation: 'CAB',
+          classes: [courseUnitClass3]
+      );
+
+      classes[courseUnit1.abbreviation] = courseUnitClass1.name;
+      classes[courseUnit2.abbreviation] = courseUnitClass2.name;
+      classes[courseUnit3.abbreviation] = courseUnitClass3.name;
+
       final ScheduleOption instance = ScheduleOption(classesSelected: classes);
+      final List<CourseUnit> courseUnits = [
+        courseUnit1,
+        courseUnit2,
+        courseUnit3
+      ];
 
       for (int i = 0; i < 7; i++) {
         if (i == 5) {
-          expect(instance.hasCollisions(i), true);
+          expect(instance.hasCollisions(i, courseUnits), true);
         } else {
-          expect(instance.hasCollisions(i), false);
+          expect(instance.hasCollisions(i, courseUnits), false);
         }
       }
     });
 
     test('Many collisions', () {
-      final Map<CourseUnit, CourseUnitClass> classes = Map();
-      final CourseUnit courseUnit1 = CourseUnit();
-      final CourseUnit courseUnit2 = CourseUnit();
-      final CourseUnit courseUnit3 = CourseUnit();
+      final Map<String, String> classes = Map();
 
-      classes[courseUnit1] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass1 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(0, '13h30', 'A'),
         LectureMock(1, '14h30', 'B'),
         LectureMock(1, '15h30', 'B'),
         LectureMock(3, '16h30', 'B'),
       ]);
-      classes[courseUnit2] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass2 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(5, '13h30', 'A'),
         LectureMock(5, '14h30', 'A'),
         LectureMock(6, '15h30', 'A'),
         LectureMock(4, '16h30', 'A'),
       ]);
-      classes[courseUnit3] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass3 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(2, '13h30', 'B'),
         LectureMock(3, '14h30', 'C'),
         LectureMock(5, '15h30', 'A'),
         LectureMock(3, '16h30', 'C'),
       ]);
+
+      final CourseUnit courseUnit1 = CourseUnit(
+          abbreviation: 'ABC',
+          classes: [courseUnitClass1]
+      );
+      final CourseUnit courseUnit2 = CourseUnit(
+          abbreviation: 'BAC',
+          classes: [courseUnitClass2]
+      );
+      final CourseUnit courseUnit3 = CourseUnit(
+          abbreviation: 'CAB',
+          classes: [courseUnitClass3]
+      );
+
+      classes[courseUnit1.abbreviation] = courseUnitClass1.name;
+      classes[courseUnit2.abbreviation] = courseUnitClass2.name;
+      classes[courseUnit3.abbreviation] = courseUnitClass3.name;
+
       final ScheduleOption instance = ScheduleOption(classesSelected: classes);
+      final List<CourseUnit> courseUnits = [
+        courseUnit1,
+        courseUnit2,
+        courseUnit3
+      ];
 
       for (int i = 0; i < 7; i++) {
         if (i == 5 || i == 1 || i == 3) {
-          expect(instance.hasCollisions(i), true);
+          expect(instance.hasCollisions(i, courseUnits), true);
         } else {
-          expect(instance.hasCollisions(i), false);
+          expect(instance.hasCollisions(i, courseUnits), false);
         }
       }
     });
 
     test('Collisions every day', () {
-      final Map<CourseUnit, CourseUnitClass> classes = Map();
-      final CourseUnit courseUnit1 = CourseUnit();
-      final CourseUnit courseUnit2 = CourseUnit();
-      final CourseUnit courseUnit3 = CourseUnit();
-      final CourseUnit courseUnit4 = CourseUnit();
+      final Map<String, String> classes = Map();
 
-      classes[courseUnit1] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass1 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(0, '13h30', 'A'),
         LectureMock(1, '14h30', 'A'),
         LectureMock(1, '15h30', 'A'),
         LectureMock(3, '16h30', 'A'),
       ]);
-      classes[courseUnit2] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass2 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(5, '13h30', 'A'),
         LectureMock(2, '14h30', 'A'),
         LectureMock(6, '15h30', 'A'),
         LectureMock(4, '16h30', 'A'),
       ]);
-      classes[courseUnit3] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass3 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(2, '13h30', 'A'),
         LectureMock(3, '14h30', 'A'),
         LectureMock(5, '15h30', 'A'),
         LectureMock(3, '16h30', 'A'),
       ]);
-      classes[courseUnit4] = CourseUnitClass(lectures: [
+      final CourseUnitClass courseUnitClass4 = CourseUnitClass(
+          name: '3LEIC01',
+          lectures: [
         LectureMock(0, '13h30', 'A'),
         LectureMock(4, '14h30', 'A'),
         LectureMock(4, '15h30', 'A'),
         LectureMock(6, '16h30', 'A'),
       ]);
+
+      final CourseUnit courseUnit1 = CourseUnit(
+          abbreviation: 'ABC',
+          classes: [courseUnitClass1]
+      );
+      final CourseUnit courseUnit2 = CourseUnit(
+          abbreviation: 'BAC',
+          classes: [courseUnitClass2]
+      );
+      final CourseUnit courseUnit3 = CourseUnit(
+          abbreviation: 'CAB',
+          classes: [courseUnitClass3]
+      );
+      final CourseUnit courseUnit4 = CourseUnit(
+          abbreviation: 'CBA',
+          classes: [courseUnitClass4]
+      );
+
+      classes[courseUnit1.abbreviation] = courseUnitClass1.name;
+      classes[courseUnit2.abbreviation] = courseUnitClass2.name;
+      classes[courseUnit3.abbreviation] = courseUnitClass3.name;
+      classes[courseUnit4.abbreviation] = courseUnitClass4.name;
+
       final ScheduleOption instance = ScheduleOption(classesSelected: classes);
+      final List<CourseUnit> courseUnits = [
+        courseUnit1,
+        courseUnit2,
+        courseUnit3,
+        courseUnit4
+      ];
 
       for (int i = 0; i < 7; i++) {
-        expect(instance.hasCollisions(i), true);
+        expect(instance.hasCollisions(i, courseUnits), true);
       }
     });
   });
