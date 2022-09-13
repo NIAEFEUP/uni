e7adbd137b0f29493b66e43e18fbcd9bd5615edc
Bruno Gomes <52865953+brunogomes30@users.noreply.github.com>
2022-09-13 17:23:25 +0100

    Fix system theme bug on locations (#560)

--- uni/lib/view/Widgets/location_marker_popup.dart
+++ uni/lib/view/Widgets/location_marker_popup.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:uni/model/entities/location.dart';
 import 'package:uni/model/entities/location_group.dart';
-import 'package:uni/view/theme_notifier.dart';
+import 'package:uni/view/Widgets/faculty_maps/faculty_maps.dart';
 
 class LocationMarkerPopup extends StatelessWidget {
   const LocationMarkerPopup(this.locationGroup,
@@ -14,7 +13,10 @@ class LocationMarkerPopup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Theme.of(context).backgroundColor.withOpacity(0.8),
+      color: Theme
+          .of(context)
+          .backgroundColor
+          .withOpacity(0.8),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15),
       ),
@@ -24,8 +26,8 @@ class LocationMarkerPopup extends StatelessWidget {
             direction: Axis.vertical,
             spacing: 8,
             children: (showId
-                    ? <Widget>[Text(locationGroup.id.toString())]
-                    : <Widget>[]) +
+                ? <Widget>[Text(locationGroup.id.toString())]
+                : <Widget>[]) +
                 buildFloors(context),
           )),
     );
@@ -34,7 +36,7 @@ class LocationMarkerPopup extends StatelessWidget {
   List<Widget> buildFloors(BuildContext context) {
     //Sort by floor
     final List<MapEntry<int, List<Location>>> entries =
-        locationGroup.floors.entries.toList();
+    locationGroup.floors.entries.toList();
     entries.sort((current, next) => -current.key.compareTo(next.key));
 
     return entries.map((entry) {
@@ -45,56 +47,47 @@ class LocationMarkerPopup extends StatelessWidget {
     }).toList();
   }
 
-  List<Widget> buildFloor(
-      BuildContext context, floor, List<Location> locations) {
-    final themeNotifier = Provider.of<ThemeNotifier>(context);
-    final Color color;
-    switch (themeNotifier.getTheme()) {
-      case ThemeMode.light:
-        color = Theme.of(context).colorScheme.primary;
-        break;
-      case ThemeMode.dark:
-        color = Theme.of(context).colorScheme.onTertiary;
-        break;
-      default:
-        color = Theme.of(context).colorScheme.primary;
-    }
+  List<Widget> buildFloor(BuildContext context, floor,
+      List<Location> locations) {
+    final Color fontColor = FacultyMaps.getFontColor(context);
 
     final String floorString =
-        0 <= floor && floor <= 9 //To maintain layout of popup
-            ? ' $floor'
-            : '$floor';
+    0 <= floor && floor <= 9 //To maintain layout of popup
+        ? ' $floor'
+        : '$floor';
 
     final Widget floorCol = Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Container(
             padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
-            child: Text('Andar $floorString', style: TextStyle(color: color)))
+            child: Text(
+                'Andar $floorString', style: TextStyle(color: fontColor)))
       ],
     );
     final Widget locationsColumn = Container(
         padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
         decoration:
-            BoxDecoration(border: Border(left: BorderSide(color: color))),
+        BoxDecoration(border: Border(left: BorderSide(color: fontColor))),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: buildLocations(context, locations, color),
+          children: buildLocations(context, locations, fontColor),
         ));
     return [floorCol, locationsColumn];
   }
 
-  List<Widget> buildLocations(
-      BuildContext context, List<Location> locations, Color color) {
+  List<Widget> buildLocations(BuildContext context, List<Location> locations,
+      Color color) {
     return locations
-        .map((location) => Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(location.description(),
-                    textAlign: TextAlign.left, style: TextStyle(color: color))
-              ],
-            ))
+        .map((location) =>
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(location.description(),
+                textAlign: TextAlign.left, style: TextStyle(color: color))
+          ],
+        ))
         .toList();
   }
 }
