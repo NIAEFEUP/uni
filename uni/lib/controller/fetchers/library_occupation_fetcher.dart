c3e186ef39b7a28479c35cd476f0230c920bc2e4
luis <joseluiscunharodrigues@gmail.com>
2022-10-19 14:10:47 +0100

    Switched google sheets accessing method

--- uni/lib/controller/fetchers/library_occupation_fetcher.dart
+++ uni/lib/controller/fetchers/library_occupation_fetcher.dart
@@ -2,39 +2,35 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:gsheets/gsheets.dart';
+import 'package:http/http.dart';
 import 'package:redux/redux.dart';
+import 'package:uni/controller/fetchers/session_dependant_fetcher.dart';
+import 'package:uni/controller/networking/network_router.dart';
 import 'package:uni/controller/parsers/parser_library_occupation.dart';
 import 'package:uni/model/app_state.dart';
 import 'package:uni/model/entities/library_occupation.dart';
+import 'package:uni/model/entities/session.dart';
 
 /// Fetch the library occupation from Google Sheets
-class LibraryOccupationFetcherSheets {
+class LibraryOccupationFetcherSheets implements SessionDependantFetcher {
+  @override
+  List<String> getEndpoints(Session session) {
+    // TO DO: Implement parsers for all faculties
+    // and dispatch for different fetchers
+    const String base_url = 'https://docs.google.com/spreadsheets/d/';
+    const String sheet_id = '1gZRbEX4y8vNW7vrl15FCdAQ3pVNRJw_uRZtVL6ORP0g';
+    const String url = '$base_url$sheet_id/gviz/tq?tqx=out:json';
+    return [url];
+  }
+
   Future<LibraryOccupation> getLibraryOccupationFromSheets(
       Store<AppState> store) async {
-    const sheetId = '1gZRbEX4y8vNW7vrl15FCdAQ3pVNRJw_uRZtVL6ORP0g';
-
-    try {
-      final Map<String, dynamic> key = await loadApiKey();
-      final gSheets = GSheets(key);
-      final ss = await gSheets.spreadsheet(sheetId);
-
-      final sheet = ss.worksheetByTitle('MANUAL');
-
-      return getLibraryOccupationFromSheet(sheet!);
-    } catch (FlutterError) {
-      return LibraryOccupation(0, 0);
-    }
-  }
-
-  Future<Map<String, dynamic>> parseJsonFromAssets(String assetsPath) async {
-    return rootBundle
-        .loadString(assetsPath)
-        .then((jsonStr) => jsonDecode(jsonStr));
-  }
-
-  Future<Map<String, dynamic>> loadApiKey() async {
-    final Map<String, dynamic> dataMap =
-        await parseJsonFromAssets('assets/env/env.json');
-    return dataMap['sheets'];
+    final Session session = store.state.content['session'];
+    final String url = getEndpoints(session)[0];
+    final Future<Response> response =
+        NetworkRouter.getWithCookies(url, {}, session);
+    final LibraryOccupation occupation = await response
+        .then((response) => parseLibraryOccupationFromSheets(response));
+    return occupation;
   }
 }
--- uni/lib/controller/parsers/parser_library_occupation.dart
+++ uni/lib/controller/parsers/parser_library_occupation.dart
@@ -1,19 +1,24 @@
+import 'dart:convert';
+
 import 'package:gsheets/gsheets.dart';
+import 'package:html/parser.dart';
+import 'package:http/http.dart';
 import 'package:uni/model/entities/library_occupation.dart';
 
-Future<LibraryOccupation> getLibraryOccupationFromSheet(Worksheet sheet) async {
-  try {
-    final occupation = LibraryOccupation(0, 0);
+Future<LibraryOccupation> parseLibraryOccupationFromSheets(
+    Response response) async {
+  final js = response.body.split('\n')[1];
+  const skip = 'google.visualization.Query.setResponse(';
+  const num_floors = 6;
+  const List<int> max = [69, 105, 105, 105, 40, 64];
+  LibraryOccupation occupation = LibraryOccupation(0, 0);
 
-    for (int i = 1; i < 7; i++) {
-      final occupationCell = await sheet.cells.cell(column: 3, row: i + 1);
-      final capacityCell = await sheet.cells.cell(column: 5, row: i + 1);
+  final json = jsonDecode(js.substring(skip.length, js.length - 2));
 
-      occupation.addFloor(FloorOccupation(
-          i, int.parse(occupationCell.value), int.parse(capacityCell.value)));
-    }
-    return occupation;
-  } catch (FormatException) {
-    return LibraryOccupation(0, 0);
+  for (int i = 0; i < num_floors; i++) {
+    int floor = json["table"]["rows"][i]["c"][8]["v"].toInt();
+    occupation.addFloor(FloorOccupation(i + 1, floor, max[i]));
   }
+
+  return occupation;
 }
