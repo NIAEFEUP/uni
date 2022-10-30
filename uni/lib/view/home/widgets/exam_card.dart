f16352e5eed5ea16cb44959b6af2600293cddd50
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-10-30 19:34:38 +0000

    Add custom exam card loading widget

--- uni/lib/view/home/widgets/exam_card.dart
+++ uni/lib/view/home/widgets/exam_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:tuple/tuple.dart';
+import 'package:shimmer/shimmer.dart';
+
 import 'package:uni/model/app_state.dart';
 import 'package:uni/model/entities/exam.dart';
 import 'package:uni/view/exams/widgets/exam_title.dart';
@@ -60,6 +62,7 @@ class ExamCard extends GenericCard {
           child: Text('Não existem exames para apresentar',
               style: Theme.of(context).textTheme.headline6),
         ),
+        contentLoadingWidget: examLoadingShimmerBuilder(context),
       ),
     );
   }
@@ -142,4 +145,86 @@ class ExamCard extends GenericCard {
       ),
     );
   }
+}
+
+
+
+Widget examLoadingShimmerBuilder(BuildContext context){
+  return Center(
+        child: Shimmer.fromColors(
+          baseColor: Theme.of(context).highlightColor,
+          highlightColor: Theme.of(context).colorScheme.onPrimary,
+          child: Container(
+            padding: const EdgeInsets.only(left: 12.0, bottom: 8.0, right: 12),
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Column(
+              children: [
+                Container(
+                    margin: const EdgeInsets.only(top: 8, bottom: 8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: <Widget>[ //timestamp section
+                                      Container(
+                                        height: 15, 
+                                        width: 40, 
+                                        color: Colors.black,
+                                      ),
+                                      const SizedBox(height: 2.5,),
+                                      Container(
+                                        height: 15, 
+                                        width: 40, 
+                                        color: Colors.black,
+                                      ),
+
+                                    ],
+                                  )
+                            ]),
+                        Container(height: 30, width: 100, color: Colors.black,), //UC section
+                        Container(height: 40, width: 40, color: Colors.black,), //Calender add section
+                      ],
+                    )),
+              const SizedBox(height: 10,),
+              Row( //Exam room section
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 15, 
+                    width: 40, 
+                    color: Colors.black,
+                  ),
+                  const SizedBox(width: 10,),
+                  Container(
+                    height: 15, 
+                    width: 40, 
+                    color: Colors.black,
+                  ),
+                  const SizedBox(width: 10,),
+                  Container(
+                    height: 15, 
+                    width: 40, 
+                    color: Colors.black,
+                  ),
+                  const SizedBox(width: 10,),
+                                    Container(
+                    height: 15, 
+                    width: 40, 
+                    color: Colors.black,
+                  ),
+                ],
+              )
+              ],
+            ))));
 }
