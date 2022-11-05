31404f4be24a5a5b8acbd58a1a5cf9f47d8948e4
srissirs <sarareis2002@gmail.com>
2022-11-05 01:34:46 +0000

    fixed tests

--- uni/test/integration/src/exams_page_test.dart
+++ uni/test/integration/src/exams_page_test.dart
@@ -39,11 +39,11 @@ void main() {
     final DateTime beginSopeExam = DateTime.parse('2099-11-18 17:00');
     final DateTime endSopeExam = DateTime.parse('2099-11-18 19:00');
     final sopeExam =
-        Exam(beginSopeExam, endSopeExam, 'SOPE', [], 'MT');
+        Exam('1234',beginSopeExam, endSopeExam, 'SOPE', [], 'MT');
     final DateTime beginSdisExam = DateTime.parse('2099-10-21 17:00');
     final DateTime endSdisExam = DateTime.parse('2099-10-21 19:00');
     final sdisExam =
-        Exam(beginSdisExam, endSdisExam, 'SDIS',[], 'MT');
+        Exam('1233',beginSdisExam, endSdisExam, 'SDIS',[], 'MT');
 
     final Map<String, bool> filteredExams = {};
     Exam.types
