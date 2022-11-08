1c1332765bf36df2cad465e1c8438718c35d0d07
coutinho21 <guilhermecscoutinho@gmail.com>
2022-11-08 19:09:54 +0000

    Added error and success methods to ToastMessage

--- uni/lib/view/common_widgets/toast_message.dart
+++ uni/lib/view/common_widgets/toast_message.dart
@@ -5,13 +5,24 @@ import 'package:toast/toast.dart';
 ///
 /// usage example: ToastMessage.display(context, toastMsg);
 class ToastMessage {
-  static const Color toastColor = Color.fromARGB(255, 100, 100, 100);
-  static display(BuildContext context, String msg) {
+  static const Color toastErrorColor = Color.fromARGB(255, 100, 100, 100),
+                     toastSuccessColor = Color.fromARGB(255, 0, 250, 154);
+
+  static errorMessage(BuildContext context, String msg){
+    ToastContext().init(context);
+    Toast.show(msg,
+      duration: Toast.lengthLong,
+      gravity: Toast.bottom,
+      backgroundColor: toastErrorColor,
+      backgroundRadius: 16.0);
+  }
+
+  static successMessage(BuildContext context, String msg){
     ToastContext().init(context);
     Toast.show(msg,
         duration: Toast.lengthLong,
         gravity: Toast.bottom,
-        backgroundColor: toastColor,
+        backgroundColor: toastSuccessColor,
         backgroundRadius: 16.0);
   }
 }
