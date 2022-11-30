92f181f932e2ef162eff765c2f41c0c222cc33ca
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2022-11-30 11:22:30 +0000

    Add a notification timeout to prevent spam

--- uni/lib/controller/backgroundWorkers/notifications.dart
+++ uni/lib/controller/backgroundWorkers/notifications.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:logger/logger.dart';
 import 'package:tuple/tuple.dart';
 import 'package:uni/controller/local_storage/app_shared_preferences.dart';
+import 'package:uni/controller/local_storage/notification_timeout_storage.dart';
 import 'package:uni/controller/networking/network_router.dart';
 import 'package:uni/model/entities/session.dart';
 import 'package:workmanager/workmanager.dart';
@@ -13,13 +15,17 @@ import 'package:workmanager/workmanager.dart';
 /// (due to background worker limitations).
 /// 
 Map<Type, Notification Function()> notificationMap = {
-
 };
 
 
 
 
 abstract class Notification{
+  
+  String uniqueID;
+  Duration timeout;
+
+  Notification(this.uniqueID, this.timeout);
 
   Tuple2<String, String> buildNotificationContent(Session session);
 
@@ -32,14 +38,15 @@ abstract class Notification{
       displayNotification(buildNotificationContent(session));
     }
   }
-
-
-
 }
 
 class NotificationManager{
-  
+
+
+
   static Future<void> tryRunAll() async{
+    //first we get the .json file that contains the last time that the notification have ran
+    final notificationStorage = await NotificationTimeoutStorage.create();
     final userInfo = await AppSharedPreferences.getPersistentUserInfo();
     final faculties = await AppSharedPreferences.getUserFaculties();
     
@@ -48,7 +55,12 @@ class NotificationManager{
 
     notificationMap.forEach((key, value) 
       {
-        value().displayNotificationIfPossible(session);
+        final Notification notification = value();
+        final DateTime lastRan = notificationStorage.getLastTimeNotificationExecuted(notification.uniqueID);
+        if(lastRan.add(notification.timeout).isBefore(DateTime.now())) {
+          notification.displayNotificationIfPossible(session);
+          notificationStorage.addLastTimeNotificationExecuted(notification.uniqueID, DateTime.now());
+        }
       });
 
   }
@@ -57,6 +69,7 @@ class NotificationManager{
     //FIXME: using initial delay to make login sequence more consistent
     //can be fixed by only using buildNotificationWorker when user is logged in
     if(Platform.isAndroid){
+      Workmanager().cancelByUniqueName("notification-worker");
       Workmanager().cancelByUniqueName("pt.up.fe.ni.uni.notificationworker"); //stop task if it's already running
       Workmanager().registerPeriodicTask("pt.up.fe.ni.uni.notificationworker", "pt.up.fe.ni.uni.notificationworker", 
         constraints: Constraints(networkType: NetworkType.connected),
