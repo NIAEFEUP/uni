b45faa96003e59bf86082fd9a6c19f5a3f3bd684
thePeras <jtopereira@gmail.com>
2023-02-06 19:07:19 +0000

    map attribution text

--- uni/lib/view/locations/widgets/map.dart
+++ uni/lib/view/locations/widgets/map.dart
@@ -3,6 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:uni/model/entities/location_group.dart';
 import 'package:uni/view/locations/widgets/floorless_marker_popup.dart';
@@ -27,43 +28,61 @@ class LocationsMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
-      options: MapOptions(
-        minZoom: 17,
-        maxZoom: 18,
-        nePanBoundary: northEastBoundary,
-        swPanBoundary: southWestBoundary,
-        center: center,
-        zoom: 17.5,
-        rotation: 0,
-        interactiveFlags: InteractiveFlag.all - InteractiveFlag.rotate,
-        onTap: (tapPosition, latlng) => _popupLayerController.hideAllPopups(),
-      ),
-      children: <Widget>[
-        TileLayerWidget(
-          options: TileLayerOptions(
-            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            subdomains: <String>['a', 'b', 'c'],
-            tileProvider: CachedTileProvider(),
-          ),
+        options: MapOptions(
+          minZoom: 17,
+          maxZoom: 18,
+          nePanBoundary: northEastBoundary,
+          swPanBoundary: southWestBoundary,
+          center: center,
+          zoom: 17.5,
+          rotation: 0,
+          interactiveFlags: InteractiveFlag.all - InteractiveFlag.rotate,
+          onTap: (tapPosition, latlng) => _popupLayerController.hideAllPopups(),
         ),
-        PopupMarkerLayerWidget(
-          options: PopupMarkerLayerOptions(
-            markers: _getMarkers(),
-            popupController: _popupLayerController,
-            popupAnimation: const PopupAnimation.fade(
-                duration: Duration(milliseconds: 400)),
-            popupBuilder: (_, Marker marker) {
-              if (marker is LocationMarker) {
-                return marker.locationGroup.isFloorless
-                    ? FloorlessLocationMarkerPopup(marker.locationGroup)
-                    : LocationMarkerPopup(marker.locationGroup);
-              }
-              return const Card(child: Text('undefined'));
-            },
+        nonRotatedChildren: [
+          Align(
+            alignment: Alignment.bottomRight,
+            child: ColoredBox(
+              color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.8),
+              child: GestureDetector(
+                onTap: () => launchUrl(
+                    Uri(host: 'openstreetmap.org', path: '/copyright')),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Text("© OpenStreetMap"),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+        children: <Widget>[
+          TileLayerWidget(
+            options: TileLayerOptions(
+              urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              subdomains: <String>['a', 'b', 'c'],
+              tileProvider: CachedTileProvider(),
+            ),
           ),
-        ),
-      ],
-    );
+          PopupMarkerLayerWidget(
+            options: PopupMarkerLayerOptions(
+              markers: _getMarkers(),
+              popupController: _popupLayerController,
+              popupAnimation: const PopupAnimation.fade(
+                  duration: Duration(milliseconds: 400)),
+              popupBuilder: (_, Marker marker) {
+                if (marker is LocationMarker) {
+                  return marker.locationGroup.isFloorless
+                      ? FloorlessLocationMarkerPopup(marker.locationGroup)
+                      : LocationMarkerPopup(marker.locationGroup);
+                }
+                return const Card(child: Text('undefined'));
+              },
+            ),
+          ),
+        ]);
   }
 
   List<Marker> _getMarkers() {
