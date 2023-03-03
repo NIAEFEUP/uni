e0df7560a913866fda49a30ac3b7e409f7c216b4
DGoiana <diogogoianam@gmail.com>
2023-03-03 22:29:33 +0000

    Restaurants data connected to the home card

--- uni/lib/view/home/widgets/restaurant_card.dart
+++ uni/lib/view/home/widgets/restaurant_card.dart
@@ -6,6 +6,23 @@ import 'package:uni/view/common_widgets/generic_card.dart';
 import 'package:uni/view/common_widgets/request_dependent_widget_builder.dart';
 import 'package:uni/view/common_widgets/row_container.dart';
 import 'package:uni/view/home/widgets/restaurant_row.dart';
+import 'package:uni/model/entities/meal.dart';
+import 'package:uni/model/entities/restaurant.dart';
+import 'package:uni/model/utils/day_of_week.dart';
+
+
+final List<DayOfWeek> daysOfTheWeek = [
+  DayOfWeek.monday,
+  DayOfWeek.tuesday,
+  DayOfWeek.wednesday,
+  DayOfWeek.thursday,
+  DayOfWeek.friday,
+  DayOfWeek.saturday,
+  DayOfWeek.sunday
+];
+
+final int weekDay = DateTime.now().weekday;
+final offset = (weekDay > 5) ? 0 : (weekDay - 1) % daysOfTheWeek.length;
 
 class RestaurantCard extends GenericCard {
   RestaurantCard({Key? key}) : super(key: key);
@@ -13,6 +30,7 @@ class RestaurantCard extends GenericCard {
   const RestaurantCard.fromEditingInformation(
       Key key, bool editingMode, Function()? onDelete)
       : super.fromEditingInformation(key, editingMode, onDelete);
+
 
   @override
   String getTitle() => 'Cantinas';
@@ -28,40 +46,41 @@ class RestaurantCard extends GenericCard {
                 context: context,
                 status: restaurantProvider.status,
                 contentGenerator: generateRestaurant,
-                content: restaurantProvider.restaurants,
+                content: restaurantProvider.restaurants[2],
                 contentChecker: restaurantProvider.restaurants.isNotEmpty,
                 onNullContent: Center(
                     child: Text('Não existem cantinas para apresentar',
                         style: Theme.of(context).textTheme.headline4,
                         textAlign: TextAlign.center))));
+
   }
+
 
   Widget generateRestaurant(canteens, context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
-      children: [createRowFromRestaurant(context, canteens)],
+      children: [createRowFromRestaurant(context, canteens, daysOfTheWeek[offset])],
     );
   }
 
-  Widget createRowFromRestaurant(context, String canteen) {
-    // TODO: Issue #390
+  Widget createRowFromRestaurant(context, Restaurant restaurant, DayOfWeek day) {
+    final List<Meal> meals = restaurant.getMealsOfDay(day);
     return Column(children: [
-      const DateRectangle(date: ''), // TODO: Issue #390
+      DateRectangle(date: toString(day)),
       // cantine.nextSchoolDay
       Center(
           child: Container(
-              padding: const EdgeInsets.all(12.0), child: Text(canteen))),
+              padding: const EdgeInsets.all(12.0), child: Text(restaurant.name))),
       Card(
         elevation: 1,
         child: RowContainer(
             color: const Color.fromARGB(0, 0, 0, 0),
             child: RestaurantRow(
-              local: canteen,
-              meatMenu: '',
-              // TODO: Issue #390
-              fishMenu: '',
-              vegetarianMenu: '',
-              dietMenu: '',
+              local: restaurant.name,
+              meatMenu: meals.isNotEmpty ? meals[0].name : 'Prato não disponível',
+              fishMenu: meals.length > 1 ? meals[1].name : 'Prato não disponível',
+              vegetarianMenu: meals.length > 2 ? meals[2].name : 'Prato não disponível',
+              dietMenu: meals.length > 3 ? meals[3].name : 'Prato não disponível',
             )),
       ),
     ]);
