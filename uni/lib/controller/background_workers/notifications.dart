e161bea924b716ded42f8d06e0ec5c39cdc79574
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-03-09 16:12:30 +0000

    fix linting

--- uni/lib/controller/background_workers/notifications.dart
+++ uni/lib/controller/background_workers/notifications.dart
@@ -11,7 +11,6 @@ import 'package:uni/controller/local_storage/app_shared_preferences.dart';
 import 'package:uni/controller/local_storage/notification_timeout_storage.dart';
 import 'package:uni/controller/networking/network_router.dart';
 import 'package:uni/model/entities/session.dart';
-import 'package:uni/view/common_widgets/toast_message.dart';
 import 'package:workmanager/workmanager.dart';
 
 ///
