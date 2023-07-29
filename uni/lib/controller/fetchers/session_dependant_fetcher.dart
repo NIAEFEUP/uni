1d25e27cef11c2a825364b6fcd0cb0255af901df
Luís Duarte <luis.duarte.2003.13@hotmail.com>
2023-07-29 21:52:15 +0100

    order pubspec

--- uni/lib/controller/fetchers/session_dependant_fetcher.dart
+++ uni/lib/controller/fetchers/session_dependant_fetcher.dart
@@ -1,10 +1,5 @@
 import 'package:uni/model/entities/session.dart';
 
-// TODO(luisd): apparently, it's not a good practice defining an abstract class
-//  with just one function, it's better to typedef the function, we can disable
-//  rule if we want a more java-like experience.
-
-//ignore: one_member_abstracts
 abstract class SessionDependantFetcher {
   List<String> getEndpoints(Session session);
 }
