7dc87c8d1deb9ffeba7366f53a4c1edac3e1cdd6
Processing <rbruno.p.o@gmail.com>
2023-08-17 19:38:14 +0100

    Add "Consultas SASUP" link to "Outros links" card.

--- uni/lib/view/useful_info/widgets/other_links_card.dart
+++ uni/lib/view/useful_info/widgets/other_links_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uni/view/common_widgets/generic_expansion_card.dart';
-// import 'package:uni/view/useful_info/widgets/link_button.dart';
+import 'package:uni/view/useful_info/widgets/link_button.dart';
 
 /// Manages the 'Current account' section inside the user's page (accessible
 /// through the top-right widget with the user picture)
@@ -10,11 +10,15 @@ class OtherLinksCard extends GenericExpansionCard {
   @override
   Widget buildCardContent(BuildContext context) {
     return Column(
-        /*children: const [
-        // LinkButton(title: 'Impressão', link: 'https://print.up.pt')
+      children: const [
+        // LinkButton(title: 'Impressão', link: 'https://print.up.pt'),
         // TODO(Process-ing): Get fixed link
-      ],*/
-        );
+        LinkButton(
+          title: 'Consultas SASUP',
+          link: 'https://www.up.pt/portal/pt/sasup/saude/marcar-consulta/',
+        ),
+      ],
+    );
   }
 
   @override
