8b8f5ab996ca4d98895e2fc160013b7af0e4d5ac
DGoiana <diogogoianam@gmail.com>
2023-08-25 14:32:10 +0100

    Testable widget changes

--- uni/test/test_widget.dart
+++ uni/test/test_widget.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:uni/generated/l10n.dart';
+import 'package:uni/view/locale_notifier.dart';
 
 Widget testableWidget(
   Widget widget, {
   List<ChangeNotifierProvider> providers = const [],
 }) {
-  if (providers.isEmpty) return wrapWidget(widget);
-
-  return MultiProvider(providers: providers, child: wrapWidget(widget));
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider<LocaleNotifier>(
+        create: (_) => LocaleNotifier(const Locale('pt')),
+      ),
+      ...providers
+    ],
+    child: wrapWidget(widget),
+  );
 }
 
 Widget wrapWidget(Widget widget) {
@@ -21,6 +28,8 @@ Widget wrapWidget(Widget widget) {
       GlobalCupertinoLocalizations.delegate,
     ],
     supportedLocales: S.delegate.supportedLocales,
-    home: widget,
+    home: Scaffold(
+      body: widget,
+    ),
   );
 }
