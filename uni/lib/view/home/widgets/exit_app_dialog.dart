c7606215e4eaebfcb8b322fb2a52226e5446c4ca
Sanat <sanatthakur2002@gmail.com>
2023-09-13 21:01:37 +0530

    removed dialog from exitapp

--- uni/lib/view/home/widgets/exit_app_dialog.dart
+++ uni/lib/view/home/widgets/exit_app_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 /// Manages the app section displayed when the user presses the back button
 class BackButtonExitWrapper extends StatelessWidget {
@@ -12,34 +11,10 @@ class BackButtonExitWrapper extends StatelessWidget {
   final BuildContext context;
   final Widget child;
 
-  Future<void> backButton() {
-    return showDialog<void>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(
-          'Tens a certeza de que pretendes sair?',
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        actions: <Widget>[
-          ElevatedButton(
-            onPressed: () => Navigator.of(context).pop(false),
-            child: const Text('Não'),
-          ),
-          ElevatedButton(
-            onPressed: () =>
-                SystemChannels.platform.invokeMethod('SystemNavigator.pop'),
-            child: const Text('Sim'),
-          )
-        ],
-      ),
-    );
-  }
+
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () => backButton() as Future<bool>,
-      child: child,
-    );
+    return child;
   }
 }
