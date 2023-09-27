43f4872fd5f73f8ec670bd2aa7eb9faa2a50b23b
SunMoon97 <f20220757@goa.bits-pilani.ac.in>
2023-09-27 21:04:37 +0530

    removed calender link #969

--- uni/lib/view/useful_info/widgets/sigarra_links_card.dart
+++ uni/lib/view/useful_info/widgets/sigarra_links_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uni/generated/l10n.dart';
-import 'package:uni/view/common_widgets/generic_expansion_card.dart';
 import 'package:uni/view/useful_info/widgets/link_button.dart';
+import 'package:uni/view/common_widgets/generic_expansion_card.dart';
 
 /// Manages the 'Current account' section inside the user's page (accessible
 /// through the top-right widget with the user picture)
@@ -34,11 +34,11 @@ class SigarraLinksCard extends GenericExpansionCard {
           link:
               'https://sigarra.up.pt/feup/pt/inqueritos_geral.inqueritos_list',
         ),
-        LinkButton(
-          title: S.of(context).school_calendar,
-          link:
-              'https://sigarra.up.pt/feup/pt/web_base.gera_pagina?p_pagina=p%c3%a1gina%20est%c3%a1tica%20gen%c3%a9rica%20106',
-        )
+        // LinkButton(
+        //   title: S.of(context).school_calendar,
+        //   link:
+        //       'https://sigarra.up.pt/feup/pt/web_base.gera_pagina?p_pagina=p%c3%a1gina%20est%c3%a1tica%20gen%c3%a9rica%20106',
+        // )
       ],
     );
   }
