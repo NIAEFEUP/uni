8f4465cf88968f798d0830ffd8180ca6a785b5a5
André Lima <me@limwa.pt>
2023-09-29 21:18:25 +0100

    docs: update comment on api parsing for lectures

--- uni/lib/controller/parsers/parser_schedule.dart
+++ uni/lib/controller/parsers/parser_schedule.dart
@@ -32,9 +32,10 @@ Future<List<Lecture>> parseSchedule(http.Response response) async {
     final secBegin = lecture['hora_inicio'] as int;
     final subject = lecture['ucurr_sigla'] as String;
     final typeClass = lecture['tipo'] as String;
-    // TODO(luisd): this was marked as a double on the develop branch but the
-    //  tests' example api returns an integer. At the moment there are no
-    //  classes so I can't test this.
+    
+    // Note: aula_duracao returns an integer when the lecture is 1 hour long
+    // or 2 hours long and so on. When the lecture is 1.5 hours long, it
+    // returns a double, with the value 1.5. 
     final lectureDuration = lecture['aula_duracao'];
     final blocks = lectureDuration is double
       ? (lectureDuration * 2).toInt()
