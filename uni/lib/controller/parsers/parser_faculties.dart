fb61515fca4b3f43d4fb34bc0bbce576ad7f55f8
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2023-10-11 16:35:18 +0100

    probably done

--- uni/lib/controller/parsers/parser_faculties.dart
+++ uni/lib/controller/parsers/parser_faculties.dart
@@ -24,11 +24,11 @@ Future<List<String>> getStudentFaculties(Session session) async {
   );
 
   final document = parse(response.body);
-  final list = document.querySelectorAll("#conteudoinner>ul a");
+  final list = document.querySelectorAll('#conteudoinner>ul a');
 
   // user is only present in one faculty
   if (list.isEmpty) {
-    list.add(document.querySelector("a")!); // the redirection link
+    list.add(document.querySelector('a')!); // the redirection link
   }
 
   for (final el in list) {
@@ -38,47 +38,3 @@ Future<List<String>> getStudentFaculties(Session session) async {
 
   return registerFaculties;
 }
-
-/*
-final Set<String> attendingFaculties = {};
-
-  // it should be done before the calling of this func as it has valuable info
-  // for other fetcher
-  final response = await NetworkRouter.getWithCookies(
-      'https://sigarra.up.pt/feup/pt/mob_fest_geral.perfil',
-      {'pv_codigo': session.username},
-      session);
-
-  final sessionProfile = json.decode(response.body);
-
-  for (final faculty in sessionProfile['cursos']) {
-    attendingFaculties.add(faculty['inst_sigla']);
-  }
-
-  return attendingFaculties.toList();
- */
-
-/*
-
-final List<String> registerFaculties = [];
-
-    final http.Response response = await NetworkRouter.getWithCookies(
-        'https://sigarra.up.pt/up/pt/vld_entidades_geral.entidade_pagina',
-        {'pct_id': '1923456'},
-        session);
-
-    final document = parse(response.body);
-    final list = document.querySelectorAll("#conteudoinner>ul a");
-
-    // user is only present in one faculty
-    if (list.isEmpty) {
-      list.add(document.querySelector("a")!); // the redirection link
-    }
-
-    for (final el in list) {
-      final uri = el.attributes['href']!.toUri();
-      registerFaculties.add(uri.pathSegments[0]);
-    }
-
-    return registerFaculties;
- */
