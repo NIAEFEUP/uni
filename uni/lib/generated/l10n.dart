10482559ab697062de8f4a16dfc9d5c3d6d6cd73
Bruno Mendes <bd_mendes@outlook.com>
2023-12-09 20:23:35 +0000

    Improve search box design

--- uni/lib/generated/l10n.dart
+++ uni/lib/generated/l10n.dart
@@ -1347,6 +1347,16 @@ class S {
       args: [],
     );
   }
+
+  /// `Search`
+  String get search {
+    return Intl.message(
+      'Search',
+      name: 'search',
+      desc: '',
+      args: [],
+    );
+  }
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {
