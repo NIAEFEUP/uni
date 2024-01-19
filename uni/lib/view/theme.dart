5f39333fc5573f539d001469d5f8ce05c16c7a17
vitormpp <vitormanuelpp@gmail.com>
2024-01-19 18:27:32 +0000

    Simplified color scheme and made some adjustements

--- uni/lib/view/theme.dart
+++ uni/lib/view/theme.dart
@@ -36,45 +36,14 @@ ThemeData applicationLightTheme = ThemeData(
     tertiary: lightRed,
     onTertiary: Colors.black,
   ),
-  brightness: Brightness.light,
   primaryColor: darkRed,
-  textSelectionTheme: const TextSelectionThemeData(
-    selectionHandleColor: Colors.transparent,
-  ),
-  canvasColor: _mildWhite,
-  scaffoldBackgroundColor: _mildWhite,
-  cardColor: Colors.white,
-  hintColor: _lightGrey,
   dividerColor: _lightGrey,
-  indicatorColor: darkRed,
   primaryTextTheme: Typography().black.copyWith(
         headlineMedium: const TextStyle(color: _strongGrey),
         bodyLarge: const TextStyle(color: _strongGrey),
       ),
   iconTheme: const IconThemeData(color: darkRed),
   textTheme: _textTheme,
-  switchTheme: SwitchThemeData(
-    thumbColor: MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) =>
-          states.contains(MaterialState.selected) ? darkRed : null,
-    ),
-    trackColor: MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) =>
-          states.contains(MaterialState.selected) ? darkRed : null,
-    ),
-  ),
-  radioTheme: RadioThemeData(
-    fillColor: MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) =>
-          states.contains(MaterialState.selected) ? darkRed : null,
-    ),
-  ),
-  checkboxTheme: CheckboxThemeData(
-    fillColor: MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) =>
-          states.contains(MaterialState.selected) ? darkRed : null,
-    ),
-  ),
 );
 
 ThemeData applicationDarkTheme = ThemeData(
@@ -90,13 +59,7 @@ ThemeData applicationDarkTheme = ThemeData(
     tertiary: _lightGrey,
     onTertiary: _darkishBlack,
   ),
-  brightness: Brightness.dark,
-  textSelectionTheme: const TextSelectionThemeData(
-    selectionHandleColor: Colors.transparent,
-  ),
   primaryColor: _lightGrey,
-  canvasColor: _darkBlack,
-  scaffoldBackgroundColor: _darkBlack,
   cardColor: _mildBlack,
   hintColor: _darkishBlack,
   dividerColor: _strongGrey,
@@ -104,22 +67,4 @@ ThemeData applicationDarkTheme = ThemeData(
   primaryTextTheme: Typography().white,
   iconTheme: const IconThemeData(color: _lightGrey),
   textTheme: _textTheme.apply(bodyColor: _lightGrey),
-  switchTheme: SwitchThemeData(
-    trackColor: MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) =>
-          states.contains(MaterialState.selected) ? _lightGrey : null,
-    ),
-  ),
-  radioTheme: RadioThemeData(
-    fillColor: MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) =>
-          states.contains(MaterialState.selected) ? _mildBlack : null,
-    ),
-  ),
-  checkboxTheme: CheckboxThemeData(
-    fillColor: MaterialStateProperty.resolveWith<Color?>(
-      (Set<MaterialState> states) =>
-          states.contains(MaterialState.selected) ? _mildBlack : null,
-    ),
-  ),
 );
