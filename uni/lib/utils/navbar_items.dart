5618e95b3539920673162ad166df5976630ccad5
Process-ing <rbruno.p.o@gmail.com>
2024-02-09 17:36:44 +0000

    Use NavigationItem in NavbarItem.

--- uni/lib/utils/navbar_items.dart
+++ uni/lib/utils/navbar_items.dart
@@ -1,33 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:uni/utils/navigation_items.dart';
 
 enum NavbarItem {
-  navPersonalArea(Icons.home_outlined, 'area'),
-  navAcademicPath(
-    Icons.school_outlined,
-    'percurso_academico',
-  ),
-  navRestaurants(
-    Icons.local_cafe_outlined,
-    'restaurantes',
-  ),
-  navFaculty(
-    Icons.domain_outlined,
-    'faculdade',
-  ),
-  navTransports(
-    Icons.map_outlined,
-    'transportes',
-  );
+  navPersonalArea(Icons.home_outlined, NavigationItem.navPersonalArea),
+  navAcademicPath(Icons.school_outlined, NavigationItem.navAcademicPath),
+  navRestaurants(Icons.local_cafe_outlined, NavigationItem.navRestaurants),
+  navFaculty(Icons.domain_outlined, NavigationItem.navFaculty),
+  navTransports(Icons.map_outlined, NavigationItem.navTransports);
 
-  const NavbarItem(this.icon, this.route);
+  const NavbarItem(this.icon, this.item);
 
   final IconData icon;
-  final String route;
+  final NavigationItem item;
 
   BottomNavigationBarItem toBottomNavigationBarItem() {
     return BottomNavigationBarItem(
       icon: Icon(icon),
       label: '',
     );
+  }
+
+  String get route {
+    return item.route;
   }
 }
