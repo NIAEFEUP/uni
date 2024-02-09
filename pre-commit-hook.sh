e40a7a65a62fa0f61c3729c36e8c5421cef92c04
DGoiana <diogogoianam@gmail.com>
2024-02-09 17:58:53 +0000

    Fixing pre-commit auto formatting

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -3,7 +3,7 @@ mkdir -p .git/hooks #it seems that are some cases where git will not create a ho
 tee .git/hooks/pre-commit << EOF
 #!/bin/sh
 
-FILES="\$(git diff --name-only --cached | grep .*\.dart | grep -v .*\.g\.dart | grep -v .*\.mocks\.dart)"
+FILES="\$(git diff --diff-filter=d --name-only --cached | grep .*\.dart | grep -v .*\.g\.dart | grep -v .*\.mocks\.dart)"
 
 [ -z "\$FILES" ] && exit 0
 
