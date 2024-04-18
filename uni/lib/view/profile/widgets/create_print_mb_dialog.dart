31e5d29ae8e92a9a422aaf2836a41535158b0a1c
Clara Sousa <clarasousa2004@hotmail.com>
2024-04-18 15:59:44 +0100

    resolver erros

--- uni/lib/view/profile/widgets/create_print_mb_dialog.dart
+++ uni/lib/view/profile/widgets/create_print_mb_dialog.dart
@@ -123,13 +123,13 @@ Future<void> addMoneyDialog(BuildContext context) async {
 }
 
 final CurrencyTextInputFormatter formatter =
-    CurrencyTextInputFormatter(locale: 'pt-PT', decimalDigits: 2, symbol: '€ ');
+    CurrencyTextInputFormatter.currency(locale: 'pt-PT', decimalDigits: 2, symbol: '€ ');
 
 double valueTextToNumber(String value) =>
     double.parse(value.substring(0, value.length - 2).replaceAll(',', '.'));
 
 String numberToValueText(double number) =>
-    formatter.format(number.toStringAsFixed(2));
+    number.toStringAsFixed(2);
 
 Future<void> generateReference(BuildContext context, double amount) async {
   if (amount < 1) {
