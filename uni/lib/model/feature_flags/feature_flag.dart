a133fed372a23e3645efd00b3db9eceb4d94e0b4
Process-ing <rbruno.p.o@gmail.com>
2024-07-06 21:36:07 +0100

    Create GenericFeatureFlag interface

--- uni/lib/model/feature_flags/feature_flag.dart
+++ uni/lib/model/feature_flags/feature_flag.dart
@@ -1,4 +1,6 @@
-class FeatureFlag {
+import 'package:uni/model/feature_flags/generic_feature_flag.dart';
+
+class FeatureFlag extends GenericFeatureFlag {
   FeatureFlag({
     required this.code,
     required this.name,
@@ -6,13 +8,17 @@ class FeatureFlag {
     required void Function(FeatureFlag, { required bool enabled }) saveEnabled,
   }): _enabled = enabled, _saveEnabled = saveEnabled;
 
+  @override
   final String code;
+  @override
   final String name;
   bool _enabled;
   final void Function(FeatureFlag, { required bool enabled }) _saveEnabled;
 
+  @override
   bool get enabled => _enabled;
 
+  @override
   set enabled(bool enabled) {
     _saveEnabled(this, enabled: enabled);
     _enabled = enabled;
