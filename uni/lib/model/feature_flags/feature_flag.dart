269ea6524028efce93448cac786031d55bc86bf1
Process-ing <rbruno.p.o@gmail.com>
2024-07-06 21:08:59 +0100

    Restore FeatureFlag class

---
+++ uni/lib/model/feature_flags/feature_flag.dart
@@ -0,0 +1,20 @@
+class FeatureFlag {
+  FeatureFlag({
+    required this.code,
+    required this.name,
+    required bool enabled,
+    required void Function(FeatureFlag, { required bool enabled }) saveEnabled,
+  }): _enabled = enabled, _saveEnabled = saveEnabled;
+
+  final String code;
+  final String name;
+  bool _enabled;
+  final void Function(FeatureFlag, { required bool enabled }) _saveEnabled;
+
+  bool get enabled => _enabled;
+
+  set enabled(bool enabled) {
+    _saveEnabled(this, enabled: enabled);
+    _enabled = enabled;
+  }
+}
