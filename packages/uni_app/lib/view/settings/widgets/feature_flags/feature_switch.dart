416207f297408c536ddbd90aaf9f28e84726d680
Process-ing <rbruno.p.o@gmail.com>
2024-07-07 16:35:34 +0100

    Refactor feature switch

--- packages/uni_app/lib/view/settings/widgets/feature_flags/feature_switch.dart
+++ packages/uni_app/lib/view/settings/widgets/feature_flags/feature_switch.dart
@@ -1,40 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:uni/controller/local_storage/enabled_feature_controller.dart';
+import 'package:uni/model/feature_flags/feature_flag.dart';
 
-abstract class FeatureSwitch extends StatefulWidget {
-  const FeatureSwitch({required this.enabledFeatureController, super.key});
+class FeatureSwitch extends StatelessWidget {
+  const FeatureSwitch({required this.featureFlag, super.key});
 
-  final EnabledFeatureController enabledFeatureController;
-
-  @override
-  FeatureSwitchState createState();
-}
-
-abstract class FeatureSwitchState<T extends FeatureSwitch> extends State<T> {
-  FeatureSwitchState();
-
-  @override
-  void initState() {
-    super.initState();
-    value = initializeValue();
-  }
-
-  late bool value;
-  bool initializeValue();
-  Future<void> storeValue({required bool value});
-
-  Future<void> changeValue({required bool value}) async {
-    await storeValue(value: value);
-    setState(() {
-      this.value = value;
-    });
-  }
+  final FeatureFlag featureFlag;
 
   @override
   Widget build(BuildContext context) {
-    return Switch.adaptive(
-      value: value,
-      onChanged: (value) => changeValue(value: value),
+    return Switch(
+      value: featureFlag.enabled,
+      onChanged: (value) {
+       featureFlag.enabled = value; 
+      },
     );
   }
 }
