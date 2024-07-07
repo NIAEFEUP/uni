ca3a7206f5494daf681830cf33bd61f96999915d
Process-ing <rbruno.p.o@gmail.com>
2024-07-07 17:51:59 +0100

    Refactor FeatureFlag and FeatureFlagController

--- uni/lib/model/feature_flags/feature_flag.dart
+++ uni/lib/model/feature_flags/feature_flag.dart
@@ -3,24 +3,30 @@ import 'package:uni/model/feature_flags/generic_feature_flag.dart';
 class FeatureFlag extends GenericFeatureFlag {
   FeatureFlag({
     required this.code,
-    required this.name,
+    required String name,
     required bool enabled,
-    required void Function(FeatureFlag, { required bool enabled }) saveEnabled,
-  }): _enabled = enabled, _saveEnabled = saveEnabled;
+    required void Function({ required bool enabled }) saveEnabled,
+  }): _name = name, _enabled = enabled, _saveEnabled = saveEnabled;
 
   @override
   final String code;
-  @override
-  final String name;
+  String _name;
   bool _enabled;
-  final void Function(FeatureFlag, { required bool enabled }) _saveEnabled;
+  final void Function({ required bool enabled }) _saveEnabled;
+
+  @override
+  String get name => _name;
+
+  set name(String name) {
+    _name = name;
+  }
 
   @override
   bool get enabled => _enabled;
 
   @override
   set enabled(bool enabled) {
-    _saveEnabled(this, enabled: enabled);
+    _saveEnabled(enabled: enabled);
     _enabled = enabled;
   }
 }
