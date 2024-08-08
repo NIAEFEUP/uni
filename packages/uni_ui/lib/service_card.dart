2398926d4cc9bd27fcc34baefa1cc3e0c92c9222
thePeras <jtopereira@gmail.com>
2024-08-08 22:36:05 +0100

    Services Card

---
+++ packages/uni_ui/lib/service_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:uni_ui/generic_card.dart';
+
+class ServiceCard extends StatelessWidget {
+  const ServiceCard({
+    super.key,
+    required this.name,
+    required this.openingHours,
+  });
+
+  final String name;
+  final List<String> openingHours;
+
+  @override
+  Widget build(BuildContext context) {
+    return GenericCard(
+      key: key,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                name,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.headlineMedium!,
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              PhosphorIcon(
+                PhosphorIcons.clock(PhosphorIconsStyle.duotone),
+                color: Theme.of(context).textTheme.bodyMedium!.color,
+                size: 20,
+              ),
+              const SizedBox(width: 5),
+              Column(
+                children: [
+                  for (var i = 0; i < openingHours.length; i++)
+                    Text(openingHours[i],
+                        style: Theme.of(context).textTheme.bodySmall),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
