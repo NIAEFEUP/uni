84c99e72a1bb760f6abe2b61d556fa8169a4b57d
DGoiana <diogogoianam@gmail.com>
2024-08-21 23:41:00 +0100

    average bar implementation

---
+++ packages/uni_ui/lib/average_bar.dart
@@ -0,0 +1,54 @@
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:flutter/material.dart';
+
+class AverageBar extends StatelessWidget {
+  const AverageBar(
+      {required this.average,
+      required this.completedCredits,
+      required this.totalCredits,
+      super.key});
+
+  final double average;
+  final int completedCredits;
+  final int totalCredits;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Row(children: [
+        ClipSmoothRect(
+            radius: SmoothBorderRadius(
+              cornerRadius: 10,
+              cornerSmoothing: 1,
+            ),
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.all(Radius.circular(5))),
+                width: constraints.maxWidth * 0.25,
+                height: 50,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Average'),
+                      Text(average.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18))
+                    ]))),
+        SizedBox(width: constraints.maxWidth * 0.05),
+        Container(
+            width: constraints.maxWidth * 0.70,
+            child: Column(children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text('ECTS'),
+                Text('$completedCredits/$totalCredits'),
+              ]),
+              LinearProgressIndicator(
+                  minHeight: 8,
+                  value: completedCredits / totalCredits,
+                  borderRadius: BorderRadius.all(Radius.circular(5)))
+            ]))
+      ]);
+    });
+  }
+}
