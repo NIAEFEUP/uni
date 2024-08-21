911b2b12835a3a4bf635cc0e976d9ffc07dbafd7
Process-ing <rbruno.p.o@gmail.com>
2024-08-21 21:00:19 +0100

    Refactor feature flag dialog

--- packages/uni_app/lib/model/feature_flags/generic_feature_flag.dart
+++ packages/uni_app/lib/model/feature_flags/generic_feature_flag.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 abstract class GenericFeatureFlag {
   String get code;
   String getName(BuildContext context);
-  bool get enabled;
-  set enabled(bool enabled);
+  bool isEnabled();
+  Future<void> setEnabled({required bool enabled});
 }
