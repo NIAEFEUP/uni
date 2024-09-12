7acad75e3ca02757f537ef73cce51ce614df6fe0
DGoiana <diogogoianam@gmail.com>
2024-09-12 23:40:58 +0100

    changing map to list of tuples

--- packages/uni_app/lib/controller/fetchers/library_occupation_fetcher.dart
+++ packages/uni_app/lib/controller/fetchers/library_occupation_fetcher.dart
@@ -3,36 +3,37 @@ import 'dart:convert';
 import 'package:collection/collection.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:tuple/tuple.dart';
 import 'package:uni/model/entities/library_occupation.dart';
 
 /// Fetch the library occupation from Google Sheets
 class LibraryOccupationFetcher {
   String baseUrl = 'https://webapi.affluences.com/api/fillRate?';
 
-  Map<String, int> floorMaxSeats = {
-    'BruV6IlujdwAe1': 72,
-    'cEhyzJZvC5nHSr': 114,
-    'iceVfgwZWaZRhV': 114,
-    '1yLPz9X0CNsg27': 114,
-    'keu1j5zERlQn90': 40,
-    'bY7K1v43HiAq55': 90,
-  };
+  static const List<Tuple2<String, int>> floorMaxSeats = [
+    Tuple2('BruV6IlujdwAe1', 72),
+    Tuple2('cEhyzJZvC5nHSr', 114),
+    Tuple2('iceVfgwZWaZRhV', 114),
+    Tuple2('1yLPz9X0CNsg27', 114),
+    Tuple2('keu1j5zERlQn90', 40),
+    Tuple2('bY7K1v43HiAq55', 90),
+  ];
 
   Future<LibraryOccupation> getLibraryOccupation() async {
     final libraryOccupation = LibraryOccupation(0, 0);
 
     await Future.wait(
-      floorMaxSeats.entries.mapIndexed((i, entry) async {
+      floorMaxSeats.mapIndexed((i, entry) async {
         final url = Uri.parse(baseUrl).replace(
           queryParameters: {
-            'token': entry.key,
+            'token': entry.item1,
           },
         );
 
         final response = await http.get(url);
 
         final floorOccupation =
-            processFloorOccupation(response, entry.value, i);
+            processFloorOccupation(response, entry.item2, i);
 
         libraryOccupation.addFloor(floorOccupation);
       }),
