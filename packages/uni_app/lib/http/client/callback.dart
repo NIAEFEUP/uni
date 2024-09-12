dd1b60b4f73de63dbd8f5c290eb7951b4b2d5041
André Lima <me@limwa.pt>
2024-09-12 11:48:15 +0100

    fix: use inner client on callback client

--- packages/uni_app/lib/http/client/callback.dart
+++ packages/uni_app/lib/http/client/callback.dart
@@ -3,16 +3,20 @@ import 'package:http/http.dart' as http;
 class CallbackClient extends http.BaseClient {
   CallbackClient(
     http.Client inner, {
-    required Future<http.StreamedResponse> Function(http.BaseRequest) send,
+    required Future<http.StreamedResponse> Function(
+      http.Client,
+      http.BaseRequest,
+    ) send,
   })  : _inner = inner,
         _send = send;
 
   final http.Client _inner;
-  final Future<http.StreamedResponse> Function(http.BaseRequest) _send;
+  final Future<http.StreamedResponse> Function(http.Client, http.BaseRequest)
+      _send;
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) =>
-      _send(request);
+      _send(_inner, request);
 
   @override
   void close() => _inner.close();
