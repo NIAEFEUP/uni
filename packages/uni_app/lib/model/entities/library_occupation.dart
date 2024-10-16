4d8b270bff391e0bf632196e199e0ebd632e3906
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2024-10-16 16:03:55 +0100

    Switch to SplayTreeSet for automatic floor ordering

--- packages/uni_app/lib/model/entities/library_occupation.dart
+++ packages/uni_app/lib/model/entities/library_occupation.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:collection';
 
 import 'package:json_annotation/json_annotation.dart';
 
@@ -8,20 +9,19 @@ part '../../generated/model/entities/library_occupation.g.dart';
 @JsonSerializable()
 class LibraryOccupation {
   LibraryOccupation(this.occupation, this.capacity) {
-    floors = [];
+    floors = SplayTreeSet<FloorOccupation>((a, b) => a.number.compareTo(b.number));
   }
 
   factory LibraryOccupation.fromJson(Map<String, dynamic> json) =>
       _$LibraryOccupationFromJson(json);
   late int occupation;
   late int capacity;
-  late List<FloorOccupation> floors;
+  
+  @JsonKey(fromJson: _floorsFromJson, toJson: _floorsToJson)
+  late SplayTreeSet<FloorOccupation> floors;
 
   void addFloor(FloorOccupation floor) {
-    floors
-      ..add(floor)
-      ..sort((a, b) => a.number.compareTo(b.number));
-
+    floors.add(floor);
     occupation += floor.occupation;
     capacity += floor.capacity;
   }
@@ -37,10 +37,22 @@ class LibraryOccupation {
     if (floors.length < number || number < 0) {
       return FloorOccupation(0, 0, 0);
     }
-    return floors[number - 1];
+    return floors.elementAt(number - 1);
   }
 
   Map<String, dynamic> toJson() => _$LibraryOccupationToJson(this);
+
+  static SplayTreeSet<FloorOccupation> _floorsFromJson(List<dynamic> json) {
+    var set = SplayTreeSet<FloorOccupation>((a, b) => a.number.compareTo(b.number));
+    json.forEach((element) {
+      set.add(FloorOccupation.fromJson(element as Map<String, dynamic>));
+    });
+    return set;
+  }
+
+  static List<Map<String, dynamic>> _floorsToJson(SplayTreeSet<FloorOccupation> floors) {
+    return floors.map((floor) => floor.toJson()).toList();
+  }
 }
 
 /// Occupation values of a single floor
