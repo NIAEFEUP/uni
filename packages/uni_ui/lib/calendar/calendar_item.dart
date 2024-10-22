0d9025db42f68d132d5f5ce7b6bd17e421713214
Process-ing <rbruno.p.o@gmail.com>
2024-10-22 15:41:54 +0100

    Design calendar item interior

--- packages/uni_ui/lib/calendar/calendar_item.dart
+++ packages/uni_ui/lib/calendar/calendar_item.dart
@@ -1,3 +1,4 @@
+import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 
 class CalendarItem extends StatelessWidget {
@@ -14,18 +15,26 @@ class CalendarItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25),
-            blurRadius: 6,
-          ),
-        ],
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+      width: 140,
+      decoration: ShapeDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        shape: SmoothRectangleBorder(
+          borderRadius: SmoothBorderRadius(
+            cornerRadius: 15,
+            cornerSmoothing: 1,
+          )
+        )
       ),
-      child: Text(this.title),
+      child: Text(
+        this.title,
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.primary,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          height: 1,
+        ),
+      ),
     );
   }
 }
