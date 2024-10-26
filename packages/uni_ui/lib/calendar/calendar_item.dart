38786279036803284af80945a9aecb50a8381e2e
Process-ing <rbruno.p.o@gmail.com>
2024-10-26 11:32:44 +0100

    Add circle above calendar item

--- packages/uni_ui/lib/calendar/calendar_item.dart
+++ packages/uni_ui/lib/calendar/calendar_item.dart
@@ -13,34 +13,49 @@ class CalendarItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
-      width: 140,
-      decoration: ShapeDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        shape: SmoothRectangleBorder(
-          borderRadius: SmoothBorderRadius(
-            cornerRadius: 12,
-            cornerSmoothing: 1,
+    return Column(
+      children: [
+        Container(
+          width: 20,
+          height: 20,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Theme.of(context).colorScheme.primary,
+              width: 4.0,
+            )
           ),
         ),
-        shadows: [
-          BoxShadow(
-            color: Theme.of(context).colorScheme.shadow.withAlpha(0x3f),
-            blurRadius: 6,
-          )
-        ]
-      ),
-      child: Text(
-        this.title,
-        style: TextStyle(
-          color: Theme.of(context).colorScheme.primary,
-          fontSize: 16,
-          fontWeight: FontWeight.w500,
-          height: 1,
+        Container(
+          margin: const EdgeInsets.all(10),
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+          width: 140,
+          decoration: ShapeDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            shape: SmoothRectangleBorder(
+              borderRadius: SmoothBorderRadius(
+                cornerRadius: 12,
+                cornerSmoothing: 1,
+              ),
+            ),
+            shadows: [
+              BoxShadow(
+                color: Theme.of(context).colorScheme.shadow.withAlpha(0x3f),
+                blurRadius: 6,
+              )
+            ]
+          ),
+          child: Text(
+            this.title,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              height: 1,
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
