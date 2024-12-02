412f2429ba9f4a14a34f0835246b18f8cddb1ac8
jcovaa <joao.vilacova281205@gmail.com>
2024-12-02 11:48:09 +0000

    first desgin - working with the themes

--- packages/uni_app/lib/view/home/home.dart
+++ packages/uni_app/lib/view/home/home.dart
@@ -6,6 +6,7 @@ import 'package:uni/view/common_widgets/page_title.dart';
 import 'package:uni/view/common_widgets/pages_layouts/general/general.dart';
 import 'package:uni/view/common_widgets/pages_layouts/general/widgets/profile_button.dart';
 import 'package:uni/view/common_widgets/pages_layouts/general/widgets/top_navigation_bar.dart';
+import 'package:uni/view/home/widgets/connectivity_warning_card.dart';
 import 'package:uni/view/home/widgets/main_cards_list.dart';
 import 'package:uni/view/home/widgets/tracking_banner.dart';
 import 'package:uni/view/home/widgets/uni_icon.dart';
@@ -49,36 +50,41 @@ class HomePageViewState extends GeneralPageViewState {
 
   @override
   Widget? getHeader(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          PageTitle(
-            name: S.of(context).nav_title('area'),
-            center: false,
-            pad: false,
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              PageTitle(
+                name: S.of(context).nav_title('area'),
+                center: false,
+                pad: false,
+              ),
+              if (isEditing)
+                ElevatedButton(
+                  onPressed: () => setState(() {
+                    isEditing = false;
+                  }),
+                  child: Text(
+                    S.of(context).edit_on,
+                  ),
+                )
+              else
+                OutlinedButton(
+                  onPressed: () => setState(() {
+                    isEditing = true;
+                  }),
+                  child: Text(
+                    S.of(context).edit_off,
+                  ),
+                ),
+            ],
           ),
-          if (isEditing)
-            ElevatedButton(
-              onPressed: () => setState(() {
-                isEditing = false;
-              }),
-              child: Text(
-                S.of(context).edit_on,
-              ),
-            )
-          else
-            OutlinedButton(
-              onPressed: () => setState(() {
-                isEditing = true;
-              }),
-              child: Text(
-                S.of(context).edit_off,
-              ),
-            ),
-        ],
-      ),
+        ),
+        const ConnectivityWarning(),
+      ]
     );
   }
 
---
+++ packages/uni_app/lib/view/home/widgets/connectivity_warning_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uni/view/theme.dart';
+
+class ConnectivityWarning extends StatelessWidget {
+  const ConnectivityWarning({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+      child: Row(
+        children: [
+          SvgPicture.asset('assets/images/circle-alert.svg',
+          colorFilter: ColorFilter.mode(
+            Theme.of(context).primaryColor,
+            BlendMode.srcIn,
+          ),
+          width: 21,
+          height: 21,
+          ),
+          const SizedBox(width: 8),
+          Text(
+            'No Internet Connection Available',
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ],
+      ),
+    );
+  }
+}
