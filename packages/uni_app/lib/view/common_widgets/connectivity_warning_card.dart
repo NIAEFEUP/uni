e2403f10affe61885f580e877a4b48c85de61fa1
jcovaa <joao.vilacova281205@gmail.com>
2024-12-08 12:35:59 +0000

    Added language theme

--- packages/uni_app/lib/view/common_widgets/connectivity_warning_card.dart
+++ packages/uni_app/lib/view/common_widgets/connectivity_warning_card.dart
@@ -1,31 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:uni/view/theme.dart';
+
+import '../../generated/l10n.dart';
 
 class ConnectivityWarning extends StatelessWidget {
   const ConnectivityWarning({super.key});
 
-  /*
-  * TODO:
-  * Change the language of the text accordingly to the theme
-  */
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
       child: Row(
         children: [
           SvgPicture.asset('assets/images/circle-alert.svg',
-          colorFilter: ColorFilter.mode(
-            Theme.of(context).primaryColor,
-            BlendMode.srcIn,
-          ),
-          width: 21,
-          height: 21,
+            colorFilter: ColorFilter.mode(
+              Theme.of(context).primaryColor,
+              BlendMode.srcIn,
+            ),
+            width: 21,
+            height: 21,
           ),
           const SizedBox(width: 8),
           Text(
-            'No Internet Connection Available',
+            S.of(context).internet_status_exception,
             style: Theme.of(context).textTheme.bodyMedium,
           ),
         ],
