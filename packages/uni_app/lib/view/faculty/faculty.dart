06449ffb20c82098156a4c1d8ddc72964dc4000c
jcovaa <joao.vilacova281205@gmail.com>
2024-12-08 12:47:29 +0000

    Added widget to other pages

--- packages/uni_app/lib/view/faculty/faculty.dart
+++ packages/uni_app/lib/view/faculty/faculty.dart
@@ -4,6 +4,7 @@ import 'package:uni/generated/l10n.dart';
 import 'package:uni/model/providers/lazy/library_occupation_provider.dart';
 import 'package:uni/utils/navigation_items.dart';
 import 'package:uni/view/calendar/widgets/calendar_card.dart';
+import 'package:uni/view/common_widgets/connectivity_warning_card.dart';
 import 'package:uni/view/common_widgets/generic_expansion_card.dart';
 import 'package:uni/view/common_widgets/pages_layouts/general/general.dart';
 import 'package:uni/view/faculty/widgets/academic_services_card.dart';
@@ -31,6 +32,7 @@ class FacultyPageViewState extends GeneralPageViewState {
   Widget getBody(BuildContext context) {
     return ListView(
       children: [
+        const ConnectivityWarning(),
         LibraryOccupationCard(),
         CalendarCard(),
         ...getUtilsSection(),
