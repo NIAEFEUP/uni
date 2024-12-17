4dcacf43008d751183d3b14dff4194d3ad37f4a3
anabela17 <ana.b.bilhoto@gmail.com>
2024-12-17 18:12:08 +0000

    serialization

--- packages/uni_app/lib/controller/local_storage/database/app_course_units_database.dart
+++ packages/uni_app/lib/controller/local_storage/database/app_course_units_database.dart
@@ -22,23 +22,8 @@ class AppCourseUnitsDatabase extends AppDatabase<List<CourseUnit>> {
     final db = await getDatabase();
     final List<Map<String, dynamic>> maps = await db.query('course_units');
     return List.generate(maps.length, (i) {
-      return CourseUnit(
-        id: maps[i]['ucurr_id'] as int?,
-        code: maps[i]['ucurr_codigo'] as String,
-        abbreviation: maps[i]['ucurr_sigla'] as String,
-        name: maps[i]['ucurr_nome'] as String,
-        curricularYear: maps[i]['ano'] as int?,
-        occurrId: maps[i]['ocorr_id'] as int,
-        semesterCode: maps[i]['per_codigo'] as String?,
-        semesterName: maps[i]['per_nome'] as String?,
-        type: maps[i]['tipo'] as String?,
-        status: maps[i]['estado'] as String?,
-        grade: maps[i]['resultado_melhor'] as String?,
-        ectsGrade: maps[i]['ectsGrade'] as String?,
-        result: maps[i]['resultado_insc'] as String?,
-        ects: maps[i]['creditos_ects'] as double?,
-        schoolYear: maps[i]['schoolYear'] as String?,
-      );
+      return CourseUnit.fromJson(maps[i]);
+
     });
   }
 
