2149b81c4137bd8b8e8de9bcbe974f4459c3dcc3
jcovaa <joao.vilacova281205@gmail.com>
2024-12-27 10:53:11 +0000

    Message with red color

--- packages/uni_app/lib/view/common_widgets/connectivity_warning_card.dart
+++ packages/uni_app/lib/view/common_widgets/connectivity_warning_card.dart
@@ -10,7 +10,23 @@ class ConnectivityWarning extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      /*  Image with background (delete if necessary)
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).cardColor,
+          boxShadow: const [
+            BoxShadow(
+            color: Color.fromARGB(0x1c, 0, 0, 0),
+            blurRadius: 7,
+            offset: Offset(0, 1),
+            ),
+          ]
+      ,),
+
+       */
       padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+      alignment: Alignment.center,
       child: Row(
         children: [
           SvgPicture.asset('assets/images/circle-alert.svg',
@@ -24,7 +40,10 @@ class ConnectivityWarning extends StatelessWidget {
           const SizedBox(width: 8),
           Text(
             S.of(context).internet_status_exception,
-            style: Theme.of(context).textTheme.bodyMedium,
+            //style: Theme.of(context).textTheme.bodyMedium,
+            style: TextStyle(
+             color: Theme.of(context).primaryColor
+           ),
           ),
         ],
       ),
