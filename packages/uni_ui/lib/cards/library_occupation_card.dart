6fcd6a878ef782713af94b1f6f9c8bfd6ea96e36
Pedro Castro <falecompedroac@gmail.com>
2024-12-30 19:49:36 +0000

    small adjustments

--- packages/uni_ui/lib/cards/library_occupation_card.dart
+++ packages/uni_ui/lib/cards/library_occupation_card.dart
@@ -11,33 +11,33 @@ class LibraryOccupationCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6),
+      padding: const EdgeInsets.symmetric(vertical: 2),
       child: Container(
         decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          borderRadius: BorderRadius.circular(23),
+          color: Theme.of(context).cardTheme.color,
+          borderRadius: BorderRadius.circular(50),
           boxShadow: const [
             BoxShadow(
               color: Color.fromARGB(0x1c, 0, 0, 0),
-              blurRadius: 35,
-              offset: Offset(8, 8),
+              blurRadius: 10,
+              offset: Offset(0, 1),
             ),
           ],
         ),
         child: Padding(
-          padding: const EdgeInsets.all(25.0),
+          padding: const EdgeInsets.all(20),
           child: Row(
             children: [
               Align(
                 alignment: Alignment.centerLeft,
                 child: CircularPercentIndicator(
-                  radius: 80,
+                  radius: 90,
                   lineWidth: 10,
                   percent: occupation.percentage / 100,
                   center: Text('${occupation.percentage}%',
                       style: Theme.of(context).textTheme.displayMedium),
-                  circularStrokeCap: CircularStrokeCap.square,
-                  backgroundColor: Theme.of(context).dividerColor,
+                  circularStrokeCap: CircularStrokeCap.round,
+                  backgroundColor: Theme.of(context).colorScheme.surface,
                   progressColor: Theme.of(context).primaryColor,
                 ),
               ),
@@ -47,7 +47,7 @@ class LibraryOccupationCard extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: occupation.floors.map((floor) {
                     return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      padding: const EdgeInsets.symmetric(vertical: 2),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -56,20 +56,21 @@ class LibraryOccupationCard extends StatelessWidget {
                             children: [
                               Text(
                                 'Floor ${floor.number}',
-                                style: Theme.of(context).textTheme.bodyLarge,
+                                style: Theme.of(context).textTheme.titleMedium,
                               ),
                               Text(
                                 '${floor.occupation}/${floor.capacity}',
-                                style: Theme.of(context).textTheme.bodyLarge,
+                                style: Theme.of(context).textTheme.titleMedium,
                               ),
                             ],
                           ),
                           const SizedBox(height: 5),
                           LinearPercentIndicator(
-                            width: 200,
+                            width: MediaQuery.of(context).size.width * 0.45,
                             lineHeight: 8.0,
                             percent: floor.percentage / 100,
-                            backgroundColor: Theme.of(context).dividerColor,
+                            backgroundColor:
+                                Theme.of(context).colorScheme.surface,
                             progressColor: Theme.of(context).primaryColor,
                             barRadius: const Radius.circular(10),
                             alignment: MainAxisAlignment.start,
