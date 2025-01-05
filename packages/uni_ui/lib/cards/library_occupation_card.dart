bfd62fd2d6d60d90ec4f78b24095b5a129036f35
Pedro Castro <falecompedroac@gmail.com>
2025-01-05 12:44:07 +0000

    changed to GenericCard

--- packages/uni_ui/lib/cards/library_occupation_card.dart
+++ packages/uni_ui/lib/cards/library_occupation_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uni_ui/cards/generic_card.dart';
 import 'package:uni_ui/model/entities/library_occupation.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
@@ -10,80 +11,60 @@ class LibraryOccupationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardTheme.color,
-          borderRadius: BorderRadius.circular(50),
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromARGB(0x1c, 0, 0, 0),
-              blurRadius: 10,
-              offset: Offset(0, 1),
-            ),
-          ],
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CircularPercentIndicator(
-                  radius: 90,
-                  lineWidth: 10,
-                  percent: occupation.percentage / 100,
-                  center: Text('${occupation.percentage}%',
-                      style: Theme.of(context).textTheme.displayMedium),
-                  circularStrokeCap: CircularStrokeCap.round,
-                  backgroundColor: Theme.of(context).colorScheme.surface,
-                  progressColor: Theme.of(context).primaryColor,
-                ),
-              ),
-              const SizedBox(width: 25),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: occupation.floors.map((floor) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 2),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+    return GenericCard(
+      key: key,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircularPercentIndicator(
+            radius: 90,
+            lineWidth: 10,
+            percent: occupation.percentage / 100,
+            center: Text('${occupation.percentage}%',
+                style: Theme.of(context).textTheme.displayMedium),
+            circularStrokeCap: CircularStrokeCap.round,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            progressColor: Theme.of(context).primaryColor,
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: occupation.floors.map((floor) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(vertical: 2),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                'Floor ${floor.number}',
-                                style: Theme.of(context).textTheme.titleMedium,
-                              ),
-                              Text(
-                                '${floor.occupation}/${floor.capacity}',
-                                style: Theme.of(context).textTheme.titleMedium,
-                              ),
-                            ],
+                          Text(
+                            'Floor ${floor.number}',
+                            style: Theme.of(context).textTheme.titleMedium,
                           ),
-                          const SizedBox(height: 5),
-                          LinearPercentIndicator(
-                            width: MediaQuery.of(context).size.width * 0.45,
-                            lineHeight: 8.0,
-                            percent: floor.percentage / 100,
-                            backgroundColor:
-                                Theme.of(context).colorScheme.surface,
-                            progressColor: Theme.of(context).primaryColor,
-                            barRadius: const Radius.circular(10),
-                            alignment: MainAxisAlignment.start,
+                          Text(
+                            '${floor.occupation}/${floor.capacity}',
+                            style: Theme.of(context).textTheme.titleMedium,
                           ),
                         ],
                       ),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ],
+                      const SizedBox(height: 5),
+                      LinearPercentIndicator(
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        lineHeight: 8.0,
+                        percent: floor.percentage / 100,
+                        backgroundColor: Theme.of(context).colorScheme.surface,
+                        progressColor: Theme.of(context).primaryColor,
+                        barRadius: const Radius.circular(10),
+                        alignment: MainAxisAlignment.start,
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
