a609efd79222420189f67f1090f7478c8990e326
thePeras <jtopereira@gmail.com>
2025-01-06 09:49:10 +0000

    Fix en intl file

--- packages/uni_app/lib/utils/navbar_items.dart
+++ packages/uni_app/lib/utils/navbar_items.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:uni/generated/l10n.dart';
 import 'package:uni/utils/navigation_items.dart';
 import 'package:uni_ui/icons.dart';
 
 enum NavbarItem {
-  // TODO(thePeras): Remove duplication
   navPersonalArea(
     UniIcons.home,
     NavigationItem.navPersonalArea,
