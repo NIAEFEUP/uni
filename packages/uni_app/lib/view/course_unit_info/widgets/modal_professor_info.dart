791c8632720c81bab1dda55534a7b149547997a7
Simão Barbosa <117586175+simaopsbarbosa@users.noreply.github.com>
2025-01-22 19:57:16 +0000

    changed to generic modal and updated info row and person info to resemble mockups

--- packages/uni_app/lib/view/course_unit_info/widgets/modal_professor_info.dart
+++ packages/uni_app/lib/view/course_unit_info/widgets/modal_professor_info.dart
@@ -6,6 +6,9 @@ import 'package:uni/model/providers/startup/profile_provider.dart';
 import 'package:uni/model/providers/startup/session_provider.dart';
 import 'package:uni_ui/icons.dart';
 import 'package:uni_ui/modal/modal.dart';
+import 'package:uni_ui/modal/widgets/info_row.dart';
+import 'package:uni_ui/modal/widgets/person_info.dart';
+import 'package:uni_ui/theme.dart';
 
 class ProfessorInfoModal extends StatelessWidget {
   const ProfessorInfoModal(this.professor, {super.key});
@@ -19,81 +22,26 @@ class ProfessorInfoModal extends StatelessWidget {
         Column(
           children: [
             FutureBuilder<File?>(
-              builder: (context, snapshot) => CircleAvatar(
-                radius: 60,
-                backgroundImage: snapshot.hasData && snapshot.data != null
-                    ? FileImage(snapshot.data!) as ImageProvider
-                    : const AssetImage(
-                        'assets/images/profile_placeholder.png',
-                      ),
+              builder: (context, snapshot) => ModalPersonInfo(
+                name: professor.name,
+                image: snapshot.hasData && snapshot.data != null
+                    ? Image(image: FileImage(snapshot.data!))
+                    : Image.asset('assets/images/profile_placeholder.png'),
               ),
               future: ProfileProvider.fetchOrGetCachedProfilePicture(
                 session,
                 studentNumber: int.parse(professor.code),
               ),
             ),
-            const SizedBox(height: 10),
-            Text(
-              professor.name,
-              style: Theme.of(context).textTheme.headlineLarge,
+            const ModalInfoRow(
+              title: 'Email',
+              description: '[email-professor@up.pt]',
+              icon: UniIcon(UniIcons.email, color: primaryVibrant),
             ),
-            const Opacity(
-              opacity: 0.25,
-              child: Divider(color: Colors.grey),
-            ),
-            Row(
-              children: [
-                UniIcon(
-                  UniIcons.email,
-                  color: Theme.of(context).colorScheme.primary,
-                  size: 32,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 10),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Email',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    Text(
-                      '[email-professor@domain.com]',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const Opacity(
-              opacity: 0.25,
-              child: Divider(color: Colors.grey),
-            ),
-            Row(
-              children: [
-                UniIcon(
-                  UniIcons.location,
-                  color: Theme.of(context).colorScheme.primary,
-                  size: 32,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(left: 10),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Sala ',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    Text(
-                      '[sala]',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ],
-                ),
-              ],
+            const ModalInfoRow(
+              title: 'Sala',
+              description: '[sala]',
+              icon: UniIcon(UniIcons.location, color: primaryVibrant),
             ),
           ],
         ),
