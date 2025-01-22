410ac057a62a0b1e81b84dff965c3e7eec6b331c
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-01-22 19:38:36 +0000

    Class selection done.

--- packages/uni_app/lib/view/course_unit_info/widgets/course_unit_classes.dart
+++ packages/uni_app/lib/view/course_unit_info/widgets/course_unit_classes.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uni/model/entities/course_units/course_unit_class.dart';
 import 'package:uni/model/providers/startup/session_provider.dart';
-import 'package:uni/view/course_unit_info/widgets/course_unit_info_card.dart';
 import 'package:uni/view/course_unit_info/widgets/course_unit_student_row.dart';
+import 'package:uni_ui/theme.dart';
 
 class CourseUnitClassesView extends StatefulWidget {
   const CourseUnitClassesView(this.classes, {super.key});
@@ -19,53 +19,82 @@ class _CourseUnitClassesViewState extends State<CourseUnitClassesView> {
 
   @override
   Widget build(BuildContext context) {
-    final session = context.read<SessionProvider>().state!;
-    final studentNumber =
-        int.tryParse(session.username.replaceAll(RegExp(r'\D'), '')) ?? 0;
+    final sessionProvider = context.read<SessionProvider>();
+    final studentNumber = int.tryParse(
+          sessionProvider.state!.username.replaceAll(RegExp(r'\D'), ''),
+        ) ??
+        0;
 
     return SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(
-            height: 50,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.classes.length,
-              itemBuilder: (context, index) {
-                final courseUnitClass = widget.classes[index];
-                final isMyClass = courseUnitClass.students
-                    .any((student) => student.number == studentNumber);
-                final isSelected = index == selectedIndex;
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedIndex = index;
-                    });
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    alignment: Alignment.center,
-                    child: Text(
-                      isMyClass
-                          ? '${courseUnitClass.className} *'
-                          : courseUnitClass.className,
-                      style: TextStyle(
-                        fontWeight:
-                            isSelected ? FontWeight.bold : FontWeight.normal,
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Column(
-              children: widget.classes[selectedIndex].students
-                  .map((student) => CourseUnitStudentRow(student, session))
-                  .toList(),
-            ),
+          _buildClassSelector(studentNumber),
+          _buildStudentList(sessionProvider),
         ],
       ),
+    );
+  }
+
+  Widget _buildClassSelector(int studentNumber) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: SizedBox(
+        height: 55, // Adjust height to fit your design.
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: widget.classes.length,
+          itemBuilder: (context, index) {
+            final courseUnitClass = widget.classes[index];
+            final isMyClass = courseUnitClass.students
+                .any((student) => student.number == studentNumber);
+            final isSelected = index == selectedIndex;
+
+            return GestureDetector(
+              onTap: () => setState(() {
+                selectedIndex = index;
+              }),
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                decoration: BoxDecoration(
+                  color: isSelected
+                      ? lightTheme.colorScheme.primary
+                      : lightTheme.colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                alignment: Alignment.center,
+                child: Text(
+                  isMyClass
+                      ? '${courseUnitClass.className} *'
+                      : courseUnitClass.className,
+                  style: isSelected
+                      ? lightTheme.textTheme.labelMedium?.copyWith(
+                          color: lightTheme.colorScheme.onPrimary,
+                        )
+                      : lightTheme.textTheme.labelMedium,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStudentList(SessionProvider session) {
+    return Column(
+      children: widget.classes[selectedIndex].students
+          .map((student) => CourseUnitStudentRow(student, session.state!))
+          .toList(),
     );
   }
 }
