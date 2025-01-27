160c969c1767e813b73b762d36d3c35542fac096
Adriano Machado <adriano.alexandre.machado1@gmail.com>
2025-01-27 19:55:27 +0000

    Moved book_card to uni_ui

--- packages/uni_app/lib/view/course_unit_info/widgets/course_unit_sheet.dart
+++ packages/uni_app/lib/view/course_unit_info/widgets/course_unit_sheet.dart
@@ -13,12 +13,11 @@ import 'package:uni/model/providers/startup/session_provider.dart';
 import 'package:uni/view/common_widgets/generic_animated_expandable.dart';
 import 'package:uni/view/common_widgets/generic_expandable.dart';
 import 'package:uni/view/course_unit_info/widgets/modal_professor_info.dart';
+import 'package:uni_ui/cards/book_card.dart';
 import 'package:uni_ui/cards/exam_card.dart';
 import 'package:uni_ui/cards/instructor_card.dart';
 import 'package:uni_ui/cards/remaining_instructors_card.dart';
 
-const double _bookCardWidth = 135;
-const double _bookCardHeight = 140;
 const double _instructorSpacing = 8;
 const double _instructorRunSpacing = 4;
 
@@ -34,194 +33,151 @@ class CourseUnitSheetView extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: ListView(
         children: [
-          _buildInstructorsSection(context),
-          _buildExamsSection(context),
-          _buildContentCard(
-            S.of(context).program,
-            courseUnitSheet.content,
-            context,
+          _buildSection(
+            title: S.of(context).instructors,
+            content: _buildInstructorsContent(context),
+            context: context,
           ),
-          _buildContentCard(
-            S.of(context).evaluation,
-            courseUnitSheet.evaluation,
-            context,
+          _buildSection(
+            title: S.of(context).assessments,
+            content: _buildExamsContent(context),
+            context: context,
           ),
-          _buildContentCard(
-            S.of(context).frequency,
-            courseUnitSheet.frequency,
-            context,
+          _buildSection(
+            title: S.of(context).program,
+            content: HtmlWidget(
+              courseUnitSheet.content != 'null'
+                  ? courseUnitSheet.content
+                  : S.of(context).no_info,
+              textStyle: Theme.of(context).textTheme.bodyLarge,
+            ),
+            context: context,
+          ),
+          _buildSection(
+            title: S.of(context).evaluation,
+            content: HtmlWidget(
+              courseUnitSheet.evaluation != 'null'
+                  ? courseUnitSheet.evaluation
+                  : S.of(context).no_info,
+              textStyle: Theme.of(context).textTheme.bodyLarge,
+            ),
+            context: context,
+          ),
+          _buildSection(
+            title: S.of(context).frequency,
+            content: HtmlWidget(
+              courseUnitSheet.frequency != 'null'
+                  ? courseUnitSheet.frequency
+                  : S.of(context).no_info,
+              textStyle: Theme.of(context).textTheme.bodyLarge,
+            ),
+            context: context,
           ),
           if (courseUnitSheet.books.isNotEmpty)
-            _buildBibliographySection(context),
+            _buildSection(
+              title: S.of(context).bibliography,
+              content: Wrap(
+                alignment: WrapAlignment.spaceBetween,
+                children: courseUnitSheet.books.map((book) {
+                  return book.isbn.isNotEmpty
+                      ? FutureBuilder<String?>(
+                          future: BookThumbFetcher().fetchBookThumb(book.isbn),
+                          builder: (context, snapshot) {
+                            return BookCard(
+                              title: book.title,
+                              isbn: book.isbn,
+                              imageUrl: snapshot.data,
+                            );
+                          },
+                        )
+                      : BookCard(
+                          title: book.title,
+                          isbn: book.isbn,
+                          imageUrl: null,
+                        );
+                }).toList(),
+              ),
+              context: context,
+            ),
         ],
       ),
     );
   }
 
-  Widget _buildInstructorsSection(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8),
-          child: Text(
-            S.of(context).instructors,
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-        ),
-        if (courseUnitSheet.professors.length <= 4)
-          Wrap(
-            spacing: _instructorSpacing,
-            runSpacing: _instructorRunSpacing,
-            children: courseUnitSheet.professors
-                .map((instructor) => _InstructorCard(instructor: instructor))
-                .toList(),
-          )
-        else
-          AnimatedExpandable(
-            firstChild:
-                _LimitedInstructorsRow(instructors: courseUnitSheet.professors),
-            secondChild:
-                _InstructorsRow(instructors: courseUnitSheet.professors),
-          ),
-        _addDivider(),
-      ],
-    );
-  }
-
-  Widget _buildExamsSection(BuildContext context) {
+  Widget _buildSection({
+    required String title,
+    required Widget content,
+    required BuildContext context,
+  }) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 8),
           child: Text(
-            S.of(context).assessments,
+            title,
             style: Theme.of(context).textTheme.headlineLarge,
           ),
         ),
-        if (exams.isEmpty)
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: Text(
-              S.of(context).noExamsScheduled,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          )
-        else
-          SizedBox(
-            height: 100,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: exams.length,
-              separatorBuilder: (context, index) =>
-                  const SizedBox(width: _instructorSpacing),
-              itemBuilder: (context, index) => SizedBox(
-                width: 240,
-                child: ExamCard(
-                  name: exams[index].subject,
-                  acronym: exams[index].subjectAcronym,
-                  rooms: exams[index].rooms,
-                  type: exams[index].examType,
-                  startTime: exams[index].startTime,
-                  examDay: exams[index].start.day.toString(),
-                  examMonth: exams[index]
-                      .monthAcronym(PreferencesController.getLocale()),
-                  showIcon: false,
-                ),
-              ),
-            ),
-          ),
-        _addDivider(),
-      ],
-    );
-  }
-
-  Widget _buildContentCard(
-    String sectionTitle,
-    String sectionContent,
-    BuildContext context,
-  ) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12),
-      child: Column(
-        children: [
-          GenericExpandable(
-            title: sectionTitle,
-            content: HtmlWidget(
-              sectionContent != 'null' ? sectionContent : S.of(context).no_info,
-              textStyle: Theme.of(context).textTheme.bodyLarge,
-            ),
-          ),
-          _addDivider(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildBibliographySection(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          S.of(context).bibliography,
-          style: Theme.of(context).textTheme.headlineLarge,
-        ),
-        Wrap(
-          alignment: WrapAlignment.spaceBetween,
-          children: courseUnitSheet.books
-              .map((book) => _buildBookTile(context, book))
-              .toList(),
+        content,
+        const Opacity(
+          opacity: 0.25,
+          child: Divider(color: Colors.grey),
         ),
       ],
     );
   }
 
-  Widget _addDivider() {
-    return const Opacity(
-      opacity: 0.25,
-      child: Divider(color: Colors.grey),
+  Widget _buildInstructorsContent(BuildContext context) {
+    if (courseUnitSheet.professors.length <= 4) {
+      return Wrap(
+        spacing: _instructorSpacing,
+        runSpacing: _instructorRunSpacing,
+        children: courseUnitSheet.professors
+            .map((instructor) => _InstructorCard(instructor: instructor))
+            .toList(),
+      );
+    }
+
+    return AnimatedExpandable(
+      firstChild:
+          _LimitedInstructorsRow(instructors: courseUnitSheet.professors),
+      secondChild: _InstructorsRow(instructors: courseUnitSheet.professors),
     );
   }
 
-  Widget _buildBookTile(BuildContext context, Book book) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
-      child: Column(
-        children: [
-          SizedBox(
-            width: _bookCardWidth,
-            height: _bookCardHeight,
-            child: book.isbn.isNotEmpty
-                ? FutureBuilder<String?>(
-                    future: BookThumbFetcher().fetchBookThumb(book.isbn),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData && snapshot.data != null) {
-                        return Image(image: NetworkImage(snapshot.data!));
-                      } else {
-                        return const Image(
-                          image: AssetImage(
-                            'assets/images/book_placeholder.png',
-                          ),
-                        );
-                      }
-                    },
-                  )
-                : const Image(
-                    image: AssetImage(
-                      'assets/images/book_placeholder.png',
-                    ),
-                  ),
+  Widget _buildExamsContent(BuildContext context) {
+    if (exams.isEmpty) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 8),
+        child: Text(
+          S.of(context).noExamsScheduled,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      );
+    }
+
+    return SizedBox(
+      height: 100,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: exams.length,
+        separatorBuilder: (context, index) =>
+            const SizedBox(width: _instructorSpacing),
+        itemBuilder: (context, index) => SizedBox(
+          width: 240,
+          child: ExamCard(
+            name: exams[index].subject,
+            acronym: exams[index].subjectAcronym,
+            rooms: exams[index].rooms,
+            type: exams[index].examType,
+            startTime: exams[index].startTime,
+            examDay: exams[index].start.day.toString(),
+            examMonth:
+                exams[index].monthAcronym(PreferencesController.getLocale()),
+            showIcon: false,
           ),
-          SizedBox(
-            width: _bookCardWidth,
-            child: Text(
-              book.title,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
