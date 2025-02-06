6cacd9e736712151cb5c0a67be81e09c71c2f269
abutuc <andre.butuc@gmail.com>
2025-02-06 14:19:39 +0000

    fix: added required tool tip

--- packages/uni_ui/lib/cards/restaurant_card.dart
+++ packages/uni_ui/lib/cards/restaurant_card.dart
@@ -24,6 +24,7 @@ class RestaurantCard extends StatelessWidget {
     return GenericCard(
         padding: EdgeInsets.zero,
         key: key,
+        tooltip: name,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
