c8cc79372b14cddbbcdb8141d5741a70c1a60aee
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-02-09 16:38:12 +0000

    First version of the timeline in the exams page. Currently there's a visual bug.

--- packages/uni_app/lib/view/academic_path/exam_page.dart
+++ packages/uni_app/lib/view/academic_path/exam_page.dart
@@ -11,6 +11,8 @@ import 'package:uni/view/lazy_consumer.dart';
 import 'package:uni/view/locale_notifier.dart';
 import 'package:uni_ui/cards/exam_card.dart';
 import 'package:uni_ui/cards/timeline_card.dart';
+import 'package:uni_ui/theme.dart';
+import 'package:uni_ui/timeline/timeline.dart';
 
 class ExamsPage extends StatefulWidget {
   const ExamsPage({super.key});
@@ -26,89 +28,137 @@ class _ExamsPageState extends State<ExamsPage> {
 
   @override
   Widget build(BuildContext context) {
-    /*
-      If we want to filters exams again
-        filteredExamTypes[Exam.getExamTypeLong(exam.examType)] ??
-     */
-
     return LazyConsumer<ExamProvider, List<Exam>>(
-      builder: (context, exams) => ListView(
-        children: _examsByMonth(exams)
-            .entries
-            .map(
-              (entry) => Padding(
+      builder: (context, exams) {
+        // Mock exams for testing
+        exams = [
+          Exam(
+            '1',
+            DateTime.now().add(Duration(days: 5)),
+            DateTime.now().add(Duration(days: 5, hours: 2)),
+            'LCOM',
+            'Computer Laboratory',
+            ['B315', 'B316'],
+            'MT',
+            'FEUP',
+          ),
+          Exam(
+            '2',
+            DateTime.now().add(Duration(days: 10)),
+            DateTime.now().add(Duration(days: 10, hours: 2)),
+            'ES',
+            'Software Engineering',
+            ['B102'],
+            'EN',
+            'FEUP',
+          ),
+          Exam(
+            '3',
+            DateTime.now().add(Duration(days: 15)),
+            DateTime.now().add(Duration(days: 15, hours: 2)),
+            'ME',
+            'Statistical Methods',
+            ['B103'],
+            'EN',
+            'FEUP',
+          ),
+          Exam(
+            '4',
+            DateTime.now().add(Duration(days: 20)),
+            DateTime.now().add(Duration(days: 20, hours: 2)),
+            'LTW',
+            'Web Languages and Technologies',
+            ['B104'],
+            'ER',
+            'FEUP',
+          ),
+        ];
+
+        final examsByMonth = _examsByMonth(exams);
+        final tabs = exams.map((exam) {
+          return Column(
+            children: [
+              Text(
+                exam.start.shortMonth(
+                  Provider.of<LocaleNotifier>(context).getLocale(),
+                ),
+                style: lightTheme.textTheme.bodySmall,
+              ),
+              Text(
+                '${exam.start.day}',
+                style: lightTheme.textTheme.bodySmall,
+              ),
+            ],
+          );
+        }).toList();
+        final content = examsByMonth.entries.map((entry) {
+          final month = entry.key;
+          final exams = entry.value;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
                 padding: const EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      DateTime(
-                        int.parse(entry.key.split('-')[0]),
-                        int.parse(entry.key.split('-')[1]),
+                child: Text(
+                  DateTime(
+                    int.parse(month.split('-')[0]),
+                    int.parse(month.split('-')[1]),
+                  )
+                      .fullMonth(
+                        Provider.of<LocaleNotifier>(context).getLocale(),
                       )
-                          .fullMonth(
+                      .capitalize(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: exams.length,
+                itemBuilder: (context, index) {
+                  final exam = exams[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: TimelineItem(
+                      title: exam.start.day.toString(),
+                      subtitle: exam.start
+                          .shortMonth(
                             Provider.of<LocaleNotifier>(context).getLocale(),
                           )
                           .capitalize(),
-                      style: Theme.of(context).textTheme.headlineMedium,
-                    ),
-                    const SizedBox(height: 8),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemCount: entry.value.length,
-                      prototypeItem: const TimelineItem(
-                        title: '1',
-                        subtitle: 'Jan',
-                        card: ExamCard(
-                          name: 'Computer Laboratory',
-                          acronym: 'LCOM',
-                          rooms: ['B315', 'B224', 'B207'],
-                          type: 'MT',
-                          startTime: '12:00',
-                        ),
+                      isActive: _nextExam(exams) == exam,
+                      card: ExamCard(
+                        name: exam.subject,
+                        acronym: exam.subjectAcronym,
+                        rooms: exam.rooms,
+                        type: exam.examType,
+                        startTime: exam.formatTime(exam.start),
+                        isInvisible: hiddenExams.contains(exam.id),
+                        iconAction: () {
+                          setState(() {
+                            if (hiddenExams.contains(exam.id)) {
+                              hiddenExams.remove(exam.id);
+                            } else {
+                              hiddenExams.add(exam.id);
+                            }
+                            PreferencesController.saveHiddenExams(hiddenExams);
+                          });
+                        },
                       ),
-                      itemBuilder: (context, index) {
-                        final exam = entry.value[index];
-                        return TimelineItem(
-                          title: exam.start.day.toString(),
-                          subtitle: exam.start
-                              .shortMonth(
-                                Provider.of<LocaleNotifier>(context)
-                                    .getLocale(),
-                              )
-                              .capitalize(),
-                          isActive: _nextExam(exams) == exam,
-                          card: ExamCard(
-                            name: exam.subject,
-                            acronym: exam.subjectAcronym,
-                            rooms: exam.rooms,
-                            type: exam.examType,
-                            startTime: exam.formatTime(exam.start),
-                            isInvisible: hiddenExams.contains(exam.id),
-                            iconAction: () {
-                              setState(() {
-                                if (hiddenExams.contains(exam.id)) {
-                                  hiddenExams.remove(exam.id);
-                                } else {
-                                  hiddenExams.add(exam.id);
-                                }
-
-                                PreferencesController.saveHiddenExams(
-                                  hiddenExams,
-                                );
-                              });
-                            },
-                          ),
-                        );
-                      },
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-            )
-            .toList(),
-      ),
+            ],
+          );
+        }).toList();
+
+        return Column(
+          children: [
+            Expanded(child: Timeline(tabs: tabs, content: content)),
+          ],
+        );
+      },
       hasContent: (exams) => exams.isNotEmpty,
       onNullContent: Center(
         heightFactor: 1.2,
