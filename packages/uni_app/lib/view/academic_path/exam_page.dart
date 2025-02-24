dbdf420ab32b10c8ff959cc934de576cbaba71ab
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-02-24 12:11:28 +0000

    Fixed merge

--- packages/uni_app/lib/view/academic_path/exam_page.dart
+++ packages/uni_app/lib/view/academic_path/exam_page.dart
@@ -133,26 +133,27 @@ class _ExamsPageState extends State<ExamsPage> {
             ),
           );
         },
-      hasContent: (exams) => exams.isNotEmpty,
-      onNullContent: LayoutBuilder(
-        // Band-aid for allowing refresh on null content
-        builder: (context, constraints) => SingleChildScrollView(
-          physics: const AlwaysScrollableScrollPhysics(),
-          child: Container(
-            height: constraints.maxHeight, // Height of bottom navbar
-            padding: const EdgeInsets.only(bottom: bottomNavbarHeight),
-            child: Center(
-              heightFactor: 1.2,
-              child: ImageLabel(
-                imagePath: 'assets/images/vacation.png',
-                label: S.of(context).no_exams_label,
-                labelTextStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: Theme.of(context).colorScheme.primary,
+        hasContent: (exams) => exams.isNotEmpty,
+        onNullContent: LayoutBuilder(
+          // Band-aid for allowing refresh on null content
+          builder: (context, constraints) => SingleChildScrollView(
+            physics: const AlwaysScrollableScrollPhysics(),
+            child: Container(
+              height: constraints.maxHeight, // Height of bottom navbar
+              padding: const EdgeInsets.only(bottom: bottomNavbarHeight),
+              child: Center(
+                heightFactor: 1.2,
+                child: ImageLabel(
+                  imagePath: 'assets/images/vacation.png',
+                  label: S.of(context).no_exams_label,
+                  labelTextStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  sublabel: S.of(context).no_exams,
+                  sublabelTextStyle: const TextStyle(fontSize: 15),
                 ),
-                sublabel: S.of(context).no_exams,
-                sublabelTextStyle: const TextStyle(fontSize: 15),
               ),
             ),
           ),
