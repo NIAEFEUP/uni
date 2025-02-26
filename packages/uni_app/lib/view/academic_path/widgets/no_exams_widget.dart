20fde1386923d4313969ec5bdf3a3a6200e9ff42
Granja5 <ze.diogo.granja@gmail.com>
2025-02-26 00:28:47 +0000

    fix: centered exam page onNullContent's image and refactored the code to conform with the norm across the rest of the app

---
+++ packages/uni_app/lib/view/academic_path/widgets/no_exams_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:uni/generated/l10n.dart';
+import 'package:uni/view/common_widgets/expanded_image_label.dart';
+
+class NoExamsWidget extends StatelessWidget {
+  const NoExamsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageLabel(
+      imagePath: 'assets/images/vacation.png',
+      label: S.of(context).no_exams_label,
+      labelTextStyle: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 18,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      sublabel: S.of(context).no_exams,
+      sublabelTextStyle: Theme.of(context).textTheme.bodyLarge,
+    );
+  }
+}
