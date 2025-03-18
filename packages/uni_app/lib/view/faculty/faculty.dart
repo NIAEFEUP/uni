82265d59cf30e69dd7093c1972dd53bd070ff12f
Pinho13 <rafaelpinhotsilva@gmail.com>
2025-03-18 17:21:54 +0000

    More fixing

--- packages/uni_app/lib/view/faculty/faculty.dart
+++ packages/uni_app/lib/view/faculty/faculty.dart
@@ -3,11 +3,10 @@ import 'package:provider/provider.dart';
 import 'package:uni/generated/l10n.dart';
 import 'package:uni/model/providers/lazy/library_occupation_provider.dart';
 import 'package:uni/utils/navigation_items.dart';
-import 'package:uni/view/calendar/widgets/calendar_card.dart';
 import 'package:uni/view/common_widgets/pages_layouts/general/general.dart';
 import 'package:uni/view/faculty/widgets/service_cards.dart';
-import 'package:uni/view/home/widgets/library/library_home_card.dart';
 import 'package:uni/view/home/widgets/calendar/calendar_home_card.dart';
+import 'package:uni/view/home/widgets/library/library_home_card.dart';
 
 class FacultyPageView extends StatefulWidget {
   const FacultyPageView({super.key});
@@ -24,16 +23,16 @@ class FacultyPageViewState extends GeneralPageViewState {
   @override
   Widget getBody(BuildContext context) {
     return ListView(
-      children: [
-        const Padding(
+      children: const [
+        Padding(
           padding: EdgeInsets.all(16),
           child: LibraryHomeCard(),
         ),
-        const Padding(
+        Padding(
           padding: EdgeInsets.all(16),
           child: CalendarHomeCard(),
         ),
-        const AllServiceCards(),
+        AllServiceCards(),
       ],
     );
   }
