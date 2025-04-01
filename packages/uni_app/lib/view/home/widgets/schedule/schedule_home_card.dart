51f4aa08f8b4d511e8961eb5dd02cb000c364740
Granja5 <ze.diogo.granja@gmail.com>
2025-04-01 17:47:55 +0100

    home page schedule card now display teacher name and photo when active

--- packages/uni_app/lib/view/home/widgets/schedule/schedule_home_card.dart
+++ packages/uni_app/lib/view/home/widgets/schedule/schedule_home_card.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'package:uni/generated/l10n.dart';
 import 'package:uni/model/entities/lecture.dart';
 import 'package:uni/model/providers/lazy/lecture_provider.dart';
+import 'package:uni/model/providers/startup/profile_provider.dart';
+import 'package:uni/model/providers/startup/session_provider.dart';
 import 'package:uni/model/utils/time/week.dart';
 import 'package:uni/view/home/widgets/generic_home_card.dart';
 import 'package:uni/view/home/widgets/schedule/timeline_shimmer.dart';
@@ -21,7 +26,7 @@ class ScheduleHomeCard extends GenericHomecard {
   Widget buildCardContent(BuildContext context) {
     return LazyConsumer<LectureProvider, List<Lecture>>(
       builder: (context, lectures) => CardTimeline(
-        items: buildTimelineItems(lectures).sublist(0, 2),
+        items: buildTimelineItems(lectures, context).sublist(0, 2),
       ),
       hasContent: (lectures) => lectures.isNotEmpty,
       onNullContent: Text(
@@ -39,9 +44,11 @@ class ScheduleHomeCard extends GenericHomecard {
   @override
   void onClick(BuildContext context) => {};
 
-  List<TimelineItem> buildTimelineItems(List<Lecture> lectures) {
+  List<TimelineItem> buildTimelineItems(
+      List<Lecture> lectures, BuildContext context,) {
     final now = DateTime.now();
     final week = Week(start: now);
+    final session = Provider.of<SessionProvider>(context, listen: false).state!;
 
     final sortedLectures = lectures
         .where((lecture) => week.contains(lecture.startTime))
@@ -55,14 +62,38 @@ class ScheduleHomeCard extends GenericHomecard {
                 now.isAfter(element.startTime) && now.isBefore(element.endTime),
             title: DateFormat('HH:mm').format(element.startTime),
             subtitle: DateFormat('HH:mm').format(element.endTime),
-            card: ScheduleCard(
-              isActive: now.isAfter(element.startTime) &&
-                  now.isBefore(element.endTime),
-              name: element.subject,
-              acronym: element.acronym, // TODO once schedule is merged
-              room: element.room,
-              type: element.typeClass,
-            ),
+            card: now.isAfter(element.startTime) &&
+                    now.isBefore(element.endTime)
+                ? FutureBuilder<File?>(
+                    future: ProfileProvider.fetchOrGetCachedProfilePicture(
+                      session,
+                      studentNumber: element.teacherId,
+                    ),
+                    builder: (context, snapshot) {
+                      return ScheduleCard(
+                        isActive: now.isAfter(element.startTime) &&
+                            now.isBefore(element.endTime),
+                        name: element.subject,
+                        acronym: element.acronym,
+                        room: element.room,
+                        type: element.typeClass,
+                        teacherName: element.teacherName,
+                        teacherPhoto: snapshot.hasData && snapshot.data != null
+                            ? Image(image: FileImage(snapshot.data!))
+                            : Image.asset(
+                                'assets/images/profile_placeholder.png',
+                              ),
+                      );
+                    },
+                  )
+                : ScheduleCard(
+                    isActive: now.isAfter(element.startTime) &&
+                        now.isBefore(element.endTime),
+                    name: element.subject,
+                    acronym: element.acronym,
+                    room: element.room,
+                    type: element.typeClass,
+                  ),
           ),
         )
         .toList();
