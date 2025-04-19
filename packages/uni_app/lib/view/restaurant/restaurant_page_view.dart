5894cbd22d6397dd58afa21dac933a5ae149820a
jcovaa <joao.vilacova281205@gmail.com>
2025-04-19 13:16:12 +0100

    changed the if statement

--- packages/uni_app/lib/view/restaurant/restaurant_page_view.dart
+++ packages/uni_app/lib/view/restaurant/restaurant_page_view.dart
@@ -135,10 +135,13 @@ class _RestaurantPageViewState extends GeneralPageViewState<RestaurantPageView>
         final isAFavorite = favoriteRestaurants.contains(a.namePt + a.period);
         final isBFavorite = favoriteRestaurants.contains(b.namePt + b.period);
 
-        if (isAFavorite != isBFavorite) {
-          return isAFavorite ? -1 : 1;
+        if (isAFavorite == isBFavorite) {
+          return 0;
         }
-        return 0;
+        if (isAFavorite) {
+          return -1;
+        }
+        return 1;
       });
   }
 
