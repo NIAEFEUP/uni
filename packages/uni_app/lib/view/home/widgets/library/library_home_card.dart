c476d5ecc7dc6fa7a5d73fd233892bf215e5c04c
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-04-19 21:12:17 +0100

    changed ONC message to IconLabel

--- packages/uni_app/lib/view/home/widgets/library/library_home_card.dart
+++ packages/uni_app/lib/view/home/widgets/library/library_home_card.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:uni/generated/l10n.dart';
 import 'package:uni/model/entities/library_occupation.dart';
 import 'package:uni/model/providers/lazy/library_occupation_provider.dart';
+import 'package:uni/view/common_widgets/icon_label.dart';
 import 'package:uni/view/home/widgets/generic_home_card.dart';
 import 'package:uni/view/home/widgets/library/library_card_shimmer.dart';
 import 'package:uni/view/lazy_consumer.dart';
 import 'package:uni_ui/cards/library_occupation_card.dart';
+import 'package:uni_ui/icons.dart';
 
 class LibraryHomeCard extends GenericHomecard {
   const LibraryHomeCard({
@@ -27,7 +29,13 @@ class LibraryHomeCard extends GenericHomecard {
       ),
       hasContent: (libraryOccupation) => libraryOccupation.capacity > 0,
       onNullContent: Center(
-        child: Text(S.of(context).no_library_info),
+        child: IconLabel(
+          icon: const Icon(
+            UniIcons.library,
+            size: 45,
+          ),
+          label: S.of(context).no_library_info,
+        ),
       ),
       contentLoadingWidget: const ShimmerLibraryHomeCard(),
     );
