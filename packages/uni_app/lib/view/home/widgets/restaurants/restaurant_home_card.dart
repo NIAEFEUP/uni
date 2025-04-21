a7858b98a97c647472b3e633802d1789c59c578d
DGoiana <diogogoianam@gmail.com>
2025-04-21 12:25:02 +0100

    fixing lint

--- packages/uni_app/lib/view/home/widgets/restaurants/restaurant_home_card.dart
+++ packages/uni_app/lib/view/home/widgets/restaurants/restaurant_home_card.dart
@@ -29,7 +29,7 @@ class RestaurantHomeCard extends GenericHomecard {
 
   @override
   Widget buildCardContent(BuildContext context) =>
-      RestaurantSlider(onClick: onClick);
+      RestaurantSlider(onClick: onCardClick);
 }
 
 class RestaurantSlider extends StatefulWidget {
