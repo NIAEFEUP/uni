e5fa6bc26d170fa04541382900eeb20b10dce4b9
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-08-03 12:03:49 +0100

    fix: INESCTEC and INEGI use a different phrase when they are closed

--- packages/uni_app/lib/controller/fetchers/restaurant_fetcher.dart
+++ packages/uni_app/lib/controller/fetchers/restaurant_fetcher.dart
@@ -19,16 +19,17 @@ class RestaurantFetcher {
         // Detect if the restaurant is closed, and change the meal type if needed.
         final bool isClosed =
             dish.dishType.namePt == 'Carne' &&
-            dish.dish.namePt ==
-                'Unidade de Alimentação Encerrada Temporariamente';
+            (dish.dish.namePt ==
+                    'Unidade de Alimentação Encerrada Temporariamente' ||
+                dish.dish.namePt == 'ENCERRADO');
         final mealTypePt = isClosed ? 'Encerrado' : dish.dishType.namePt;
 
         // Extract the information about the meal.
         meals.add(
           Meal(
             mealTypePt,
-            dish.dish.namePt,
-            dish.dish.nameEn ?? dish.dish.namePt,
+            isClosed ? 'Encerrado' : dish.dish.namePt,
+            isClosed ? 'Closed' : dish.dish.nameEn ?? dish.dish.namePt,
             dayMenu.day,
             dbDayOfWeek: parseDateTime(dayMenu.day).index,
           ),
