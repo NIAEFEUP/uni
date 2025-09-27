65b0d71f0f29e53f0152a54f065d36e237ea08d6
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2025-09-27 14:18:29 +0100

    feat: add subtitle to course unit info to show academic year

--- packages/uni_app/lib/view/widgets/pages_layouts/secondary/secondary.dart
+++ packages/uni_app/lib/view/widgets/pages_layouts/secondary/secondary.dart
@@ -32,11 +32,14 @@ abstract class SecondaryPageViewState<T extends StatefulWidget>
   @override
   String? getTitle();
 
+  String? getSubtitle() => null;
+
   @override
   @nonVirtual
   AppTopNavbar? getTopNavbar(BuildContext context) {
     return AppTopNavbar(
       title: getTitle(),
+      subtitle: getSubtitle(),
       centerTitle: true,
       leftButton: BackButton(
         style: ButtonStyle(
