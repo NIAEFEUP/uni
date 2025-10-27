d366eeec2644b5aa3dcac9f8ea8f23c40a68d125
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-10-27 17:10:08 +0000

    organized imports

--- packages/uni_app/lib/model/providers/riverpod/lecture_provider.dart
+++ packages/uni_app/lib/model/providers/riverpod/lecture_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 import 'package:uni/controller/fetchers/schedule_fetcher/schedule_fetcher_new_api.dart';
 import 'package:uni/controller/local_storage/database/database.dart';
-import 'package:logger/logger.dart';
 import 'package:uni/model/entities/lecture.dart';
 import 'package:uni/model/providers/riverpod/cached_async_notifier.dart';
 import 'package:uni/model/providers/riverpod/session_provider.dart';
