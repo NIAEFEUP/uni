7fd4df2950cdfab63c7c2fcc88e5d377001cefbb
Simão Barbosa <simao.barbosa.05@gmail.com>
2025-10-28 10:08:55 +0000

    added bottom padding to the TAC page

--- packages/uni_app/lib/view/about/widgets/terms_and_conditions.dart
+++ packages/uni_app/lib/view/about/widgets/terms_and_conditions.dart
@@ -18,37 +18,42 @@ class TermsAndConditions extends StatelessWidget {
             termsAndConditions.hasData) {
           termsAndConditionsSaved = termsAndConditions.data;
         }
-        return MarkdownBody(
-          styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-            p: const TextStyle(
-              fontSize: 12,
-              color: Colors.black87,
-              height: 1.5,
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 30),
+          child: MarkdownBody(
+            styleSheet: MarkdownStyleSheet.fromTheme(
+              Theme.of(context),
+            ).copyWith(
+              p: const TextStyle(
+                fontSize: 12,
+                color: Colors.black87,
+                height: 1.5,
+              ),
+              h1: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              h1Align: WrapAlignment.center,
+              h2: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              h2Align: WrapAlignment.center,
+              h3: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              h2Padding: const EdgeInsets.only(top: 16, bottom: 12),
+              h3Padding: const EdgeInsets.only(top: 20, bottom: 8),
             ),
-            h1: TextStyle(
-              fontSize: 26,
-              fontWeight: FontWeight.bold,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            h1Align: WrapAlignment.center,
-            h2: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w600,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            h2Align: WrapAlignment.center,
-            h3: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            h2Padding: const EdgeInsets.only(top: 16, bottom: 12),
-            h3Padding: const EdgeInsets.only(top: 20, bottom: 8),
+            data: termsAndConditionsSaved!,
+            onTapLink: (text, url, title) async {
+              await launchUrlWithToast(context, url!);
+            },
           ),
-          data: termsAndConditionsSaved!,
-          onTapLink: (text, url, title) async {
-            await launchUrlWithToast(context, url!);
-          },
         );
       },
     );
