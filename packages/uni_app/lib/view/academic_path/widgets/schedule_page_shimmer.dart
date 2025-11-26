b2a364f1d9e86d3b08db6dd5f617eb5b3f50e008
AugustoVSoares <dacbvs@gmail.com>
2025-11-26 14:27:17 +0000

    feat: exam page and schedule page shimmers have been merged. Also rewrote them to not require a provider.

--- packages/uni_app/lib/view/academic_path/widgets/schedule_page_shimmer.dart
+++ packages/uni_app/lib/view/academic_path/widgets/schedule_page_shimmer.dart
@@ -1,120 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:uni/utils/date_time_formatter.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:uni/view/home/widgets/schedule/timeline_shimmer.dart';
-import 'package:uni_ui/timeline/timeline.dart';
-import '../../locale_notifier.dart';
+import 'package:uni_ui/cards/generic_card.dart';
 
-int getDaysInMonth(int year, int month) {
-  if (month == DateTime.february) {
-    final bool isLeapYear =
-        (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
-    return isLeapYear ? 29 : 28;
-  }
-  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  return daysInMonth[month - 1];
-}
-
-class ShimmerSchedulePage extends ConsumerWidget {
+class ShimmerSchedulePage extends StatelessWidget {
   const ShimmerSchedulePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final currentDay = DateTime.now();
-    final currentMonthLength = getDaysInMonth(
-      currentDay.year,
-      currentDay.month,
-    );
-    final nextMonthDay = currentDay.add(
-      Duration(days: currentMonthLength - currentDay.day),
-    );
-    int monthAfter = nextMonthDay.month + 1;
-    int yearAfter = nextMonthDay.year;
-
-    if (monthAfter > 12) {
-      monthAfter = 1;
-      yearAfter += 1;
-    }
-    final allDays = List.generate(
-      getDaysInMonth(currentDay.year, currentDay.month) +
-          getDaysInMonth(yearAfter, monthAfter),
-      (index) => index + 1,
-    );
-    final localeNotifier = ref.read(localeProvider.notifier);
-    final tabs =
-        allDays.map((day) {
-          final date = DateTime(
-            currentDay.year,
-            currentDay.month,
-          ).add(Duration(days: day - 1));
-          return SizedBox(
-            width: 30,
-            height: 34,
-            child: Column(
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Text(
-                    date.shortMonth(localeNotifier.getLocale()),
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
+              children: List.generate(
+                14,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(right: 4),
+                  child: SizedBox(
+                    width: 60,
+                    height: 65,
+                    child: Shimmer.fromColors(
+                      baseColor: Colors.grey[300]!,
+                      highlightColor: Colors.grey[100]!,
+                      child: const GenericCard(borderRadius: 10, tooltip: ' '),
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Text(
-                    '${date.day}', // Correctly displays the day of the month
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                  ),
-                ),
-              ],
-            ),
-          );
-        }).toList();
-    // TODO: make timeline more empty content-proof
-    return Timeline(
-      tabs: tabs,
-      content: List.generate(12, (index) {
-        final date = DateTime(
-          currentDay.year,
-          currentDay.month,
-        ).add(Duration(days: index - 1));
-        return Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.only(left: 10, bottom: 10),
-                child: Text(
-                  date.formattedDate(localeNotifier.getLocale()),
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headlineLarge,
-                  maxLines: 1,
                 ),
               ),
-              const ShimmerTimelineItem(),
-              const ShimmerTimelineItem(),
-            ],
+            ),
           ),
-        );
-      }),
-      initialTab: currentDay.day,
-      tabEnabled: List.generate(allDays.length, (index) {
-        final today = DateTime(
-          currentDay.year,
-          currentDay.month,
-          currentDay.day,
-        );
-        final tabDate = DateTime(
-          currentDay.year,
-          currentDay.month,
-        ).add(Duration(days: index));
-        return !tabDate.isBefore(today);
-      }),
+        ),
+        ...List.generate(
+          3,
+          (index) => [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                bottom: 16,
+                top: 16,
+                right: 150,
+              ),
+              child: SizedBox(
+                height: 40,
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: const GenericCard(tooltip: ' ', borderRadius: 0),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: ShimmerTimelineItem(),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: ShimmerTimelineItem(),
+            ),
+          ],
+        ).expand((element) => element),
+      ],
     );
   }
 }
