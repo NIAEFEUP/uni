4b81d0f750cc6de8c2f4c286f3b2b97ce3b02e81
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2025-12-28 00:43:26 +0000

    changes in the unit tests and added validation for json values

--- packages/uni_app/test/unit/providers/faculty_locations_provider_test.dart
+++ packages/uni_app/test/unit/providers/faculty_locations_provider_test.dart
@@ -1,16 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uni/controller/fetchers/location_fetcher/location_fetcher_asset.dart';
 import 'package:uni/controller/local_storage/preferences_controller.dart';
 import 'package:uni/model/entities/location_group.dart';
+import 'package:uni/model/entities/locations/room_group_location.dart';
 import 'package:uni/model/providers/riverpod/faculty_locations_provider.dart';
 
 class FakeLocationFetcher extends LocationFetcherAsset {
   List<LocationGroup> mockedReturn = [];
+  Exception? mockedError;
 
   @override
   Future<List<LocationGroup>> getLocations() async {
+    if(mockedError != null){
+      throw mockedError!;
+    }
     return mockedReturn;
   }
 }
@@ -36,32 +42,21 @@ void main() {
   });
 
   test('Must load locations with success using a Fake class', () async {
-    const rawJson = '''
-    {
-      "data": [
-        {
-          "id": 0,
-          "lat": 41.17,
-          "lng": -8.59,
-          "isFloorless": false,
-          "locations": {
-            "0": [
-              {
-                "id": 0, 
-                "name": "Sala B004", 
-                "type": "ROOMS", 
-                "args": {"firstRoom":"B004","lastRoom":"B007"}
-              }
-            ]
-          }
-        }
-      ]
-    }
-    ''';
+    final roomGroup = RoomGroupLocation(
+      0,      
+      'B004',  
+      'B007',  
+      locationGroupId: 0, 
+    );
 
-    final groups = await fakeFetcher.getFromJSON(rawJson);
+    final manualGroup = LocationGroup(
+      const LatLng(41.17, -8.59),
+      id: 0,
+      locations: [roomGroup], 
+    );
 
-    fakeFetcher.mockedReturn = groups;
+
+    fakeFetcher.mockedReturn = [manualGroup];
 
     container.read(locationsProvider);
 
@@ -75,30 +70,38 @@ void main() {
     expect(data, isNotEmpty);
     expect(data!.length, 1);
     expect(data.first.id, 0);
+    expect(data.first.floors[0]!.first, equals(roomGroup));
   });
 
-  test('Must not crash when given an empty list', () async {
-    fakeFetcher.mockedReturn = [];
+  test('Must not crash when given empty values', () async {
+    final manualGroup = LocationGroup(
+      const LatLng(0, 0),
+      locations: [], 
+    );
+
+    fakeFetcher.mockedReturn = [manualGroup];
 
     container.read(locationsProvider);
 
     await Future<void>.delayed(Duration.zero);
 
     final state = container.read(locationsProvider);
-
     final data = state.value;
 
-    expect(data, isEmpty);
-    expect(data, isNotNull);
+    expect(data!.first.floors, isEmpty);
   });
 
-  test('Should throw an exception when JSON is not on the right format', () {
-    const brokenJSON = '{"error": "wrong format"}'; // its missing the data key
+  test('See how provider reacts to possible erros on the fetcher', () async {
+    fakeFetcher.mockedError = Exception('Data corruption or Network failure');
 
-    expect(
-      () => fakeFetcher.getFromJSON(brokenJSON),
-      throwsA(isA<TypeError>()),
-    );
+    try {
+      await container.read(locationsProvider.future);
+    } catch (_) {
+    }
+
+    final state = container.read(locationsProvider);
+    expect(state.hasError, isTrue); 
+    expect(state.error.toString(), contains('Data corruption or Network failure'));
   });
 
   test('Should emit AsyncLoading state when initialization starts', () {
@@ -107,5 +110,55 @@ void main() {
     expect(state.isLoading, isTrue);
     expect(state.hasValue, isFalse);
     expect(state, isA<AsyncLoading<List<LocationGroup>?>>());
+  });
+
+  test('Provider must reload with new value', () async {
+    final manualGroup = LocationGroup(
+      const LatLng(0, 0),
+      locations: [], 
+    );
+
+    fakeFetcher.mockedReturn = [manualGroup];
+
+    await container.read(locationsProvider.future);
+
+    final newGroup = LocationGroup(
+      const LatLng(0, 0),
+      id: 2,
+      locations: [], 
+    );
+
+    fakeFetcher.mockedReturn = [newGroup];
+
+    container.invalidate(locationsProvider);
+    await container.read(locationsProvider.future);
+
+    final state = container.read(locationsProvider);
+    expect(state.value!.first.id, 2);
+  });
+
+  test('Must recover from an error to a success state', () async {
+    fakeFetcher.mockedError = Exception('Exception');
+    try{
+      await container.read(locationsProvider.futue);
+    }catch(_){}
+    expect(container.read(locationsProvider).hasError, isTrue);
+
+    fakeFetcher.mockedError = null;
+    
+    final manualGroup = LocationGroup(
+      const LatLng(0, 0),
+      id: 1,
+      locations: [], 
+    );
+
+    fakeFetcher.mockedReturn = [manualGroup];
+
+    container.invalidate(locationsProvider);
+    await container.read(locationsProvider.future);
+
+    final state = container.read(locationsProvider);
+
+    expect(state.value!.first.id, 1);
   });
 }
