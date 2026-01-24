ddba01e551874874e79e6e27271804d678adf1f7
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2026-01-24 15:39:21 +0000

    fix: ensure profile picture fetch only occurs if context is mounted

--- packages/uni_app/lib/view/widgets/pages_layouts/general/widgets/refresh_state.dart
+++ packages/uni_app/lib/view/widgets/pages_layouts/general/widgets/refresh_state.dart
@@ -33,9 +33,11 @@ class RefreshState extends ConsumerWidget {
                           AxisDirection.down,
                   onRefresh: () async {
                     await onRefresh();
-                    await ProfileNotifier.fetchOrGetCachedProfilePicture(
-                      ref.read(sessionProvider).value!,
-                    );
+                    if (context.mounted) {
+                      await ProfileNotifier.fetchOrGetCachedProfilePicture(
+                        ref.read(sessionProvider).value!,
+                      );
+                    }
                   },
                   child: ConstrainedBox(
                     constraints: BoxConstraints(
