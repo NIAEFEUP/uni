d52a053cb7860f78a3983ed73bda05b535d01754
muralhaplays <diogosmartins.1@gmail.com>
2026-01-30 13:34:26 +0000

    Remove the parser from the exam_provider

--- packages/uni_app/lib/model/providers/riverpod/exam_provider.dart
+++ packages/uni_app/lib/model/providers/riverpod/exam_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uni/controller/fetchers/exam_fetcher.dart';
 import 'package:uni/controller/local_storage/database/database.dart';
-import 'package:uni/controller/parsers/parser_exams.dart';
 import 'package:uni/model/entities/exam.dart';
 import 'package:uni/model/entities/profile.dart';
 import 'package:uni/model/providers/riverpod/cached_async_notifier.dart';
@@ -34,11 +33,10 @@ class ExamNotifier extends CachedAsyncNotifier<List<Exam>> {
       return null;
     }
 
-    return _fetchUserExams(ParserExams(), profile, session);
+    return _fetchUserExams(profile, session);
   }
 
   Future<List<Exam>> _fetchUserExams(
-    ParserExams parser,
     Profile profile,
     Session session,
   ) async {
