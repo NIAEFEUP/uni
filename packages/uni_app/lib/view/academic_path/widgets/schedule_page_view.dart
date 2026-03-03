a55abbe8dd11ec574b347b8c38f1adef516a48b7
Simão Barbosa <simao.barbosa.05@gmail.com>
2026-03-03 22:03:45 +0000

    fix lint

--- packages/uni_app/lib/view/academic_path/widgets/schedule_page_view.dart
+++ packages/uni_app/lib/view/academic_path/widgets/schedule_page_view.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uni/model/entities/lecture.dart';
