aa6dcc003cb2dfc1b767494e04f2db0c1e1bbbda
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2026-03-12 23:17:36 +0000

    added current account provider

---
+++ packages/uni_app/lib/model/providers/riverpod/current_account_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uni/controller/fetchers/fees_fetcher.dart';
+import 'package:uni/controller/parsers/parser_current_account.dart';
+import 'package:uni/model/providers/riverpod/session_provider.dart';
+
+final currentAccountProvider = FutureProvider.autoDispose((ref) async {
+  final session = await ref.watch(sessionProvider.future);
+  final fetcher = FeesFetcher();
+  final parser = CurrentAccountParser();
+  return fetcher.extractCurrentAccount(session!, parser);
+});
