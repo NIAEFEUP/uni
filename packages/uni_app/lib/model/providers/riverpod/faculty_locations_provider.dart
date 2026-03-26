794067fecbb27b3d3a8da3ba43d5b380b70fa8b0
jcovaa <joao.vilacova281205@gmail.com>
2026-03-26 15:29:55 +0000

    unused import

--- packages/uni_app/lib/model/providers/riverpod/faculty_locations_provider.dart
+++ packages/uni_app/lib/model/providers/riverpod/faculty_locations_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:uni/controller/fetchers/location_fetcher/location_fetcher_asset.dart';
