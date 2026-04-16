6d5ec755d3719b671b4897d64511a82a2687a98e
João Lopes <158082419+joaolopes15@users.noreply.github.com>
2026-04-16 14:46:53 +0100

    first draft of skeletonizer implementation

--- packages/uni_app/lib/view/restaurant/widgets/restaurant_page_view_shimmer.dart
+++ packages/uni_app/lib/view/restaurant/widgets/restaurant_page_view_shimmer.dart
@@ -1,50 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
-import 'package:uni_ui/common/generic_squircle.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import 'package:uni_ui/cards/restaurant_card.dart';
+import 'package:uni_ui/cards/widgets/restaurant_menu_item.dart';
 
 class ShimmerRestaurantPageView extends StatelessWidget {
   const ShimmerRestaurantPageView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Shimmer.fromColors(
-          baseColor: Colors.grey[300]!,
-          highlightColor: Colors.grey[100]!,
-          child: GenericSquircle(
-            child: Container(
-              height: 200,
-              decoration: const BoxDecoration(color: Colors.white),
+    return Skeletonizer(
+      child: ListView.separated(
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return RestaurantCard(
+            name: BoneMock.name,
+            icon: const Icon(Icons.restaurant), // Dummy icon for skeletonizer
+            isFavorite: false,
+            onFavoriteToggle: () {},
+            menuItems: List.generate(
+              3,
+              (idx) => RestaurantMenuItem(
+                name: BoneMock.title,
+                icon: const Icon(Icons.restaurant_menu),
+              ),
             ),
-          ),
-        ),
-        const SizedBox(height: 16),
-        Shimmer.fromColors(
-          baseColor: Colors.grey[300]!,
-          highlightColor: Colors.grey[100]!,
-          // Suggestion: change the speed of shimmer
-          child: GenericSquircle(
-            child: Container(
-              height: 200,
-              decoration: const BoxDecoration(color: Colors.white),
-            ),
-          ),
-        ),
-        const SizedBox(height: 16),
-        Shimmer.fromColors(
-          baseColor: Colors.grey[300]!,
-          highlightColor: Colors.grey[100]!,
-          // Suggestion: change the speed of shimmer
-          child: GenericSquircle(
-            child: Container(
-              height: 200,
-              decoration: const BoxDecoration(color: Colors.white),
-            ),
-          ),
-        ),
-        const SizedBox(height: 10),
-      ],
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(height: 10),
+      ),
     );
   }
 }
