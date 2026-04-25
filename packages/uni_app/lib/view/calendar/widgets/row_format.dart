66e76207fb25f617b925819cd224e38576789086
AlexaM2 <alexamadu23@gmail.com>
2026-04-25 18:51:33 +0100

    feat(calendar): add styled clickable cards with details popup

--- packages/uni_app/lib/view/calendar/widgets/row_format.dart
+++ packages/uni_app/lib/view/calendar/widgets/row_format.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:uni/model/entities/app_locale.dart';
 import 'package:uni/model/entities/calendar_event.dart';
 import 'package:uni_ui/cards/timeline_card.dart';
+import 'package:uni_ui/icons.dart';
+import 'package:uni_ui/modal/modal.dart';
+import 'package:uni_ui/modal/widgets/info_row.dart';
+import 'package:uni_ui/modal/widgets/service_info.dart';
 
 // TODO(thePeras): This class should be extracted up
 class RowFormat extends StatelessWidget {
@@ -15,6 +19,26 @@ class RowFormat extends StatelessWidget {
   final AppLocale locale;
   final bool isToday;
 
+  void _popUp(BuildContext context) {
+    final eventPeriod = event.formattedPeriod;
+
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return ModalDialog(
+          children: [
+            ModalServiceInfo(name: event.name, durations: const []),
+            ModalInfoRow(
+              title: 'Date',
+              description: '${eventPeriod[0]} ${eventPeriod[1]}'.trim(),
+              icon: UniIcons.calendar,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final eventperiod = event.formattedPeriod;
@@ -24,12 +48,48 @@ class RowFormat extends StatelessWidget {
       subtitle: eventperiod[1],
       titleWidth: 90,
       isActive: isToday,
-      card: Text(
-        event.name,
-        style: isToday
-            ? Theme.of(context).textTheme.titleLarge
-            : Theme.of(context).textTheme.titleMedium,
-        maxLines: 5,
+      card: GestureDetector(
+        onTap: () => _popUp(context),
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+          decoration: ShapeDecoration(
+            gradient: isToday
+                ? RadialGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.onTertiary,
+                      Theme.of(context).colorScheme.tertiary,
+                    ],
+                    center: Alignment.topLeft,
+                    radius: 2,
+                    stops: const [0, 1],
+                  )
+                : null,
+            color: isToday ? null : Theme.of(context).colorScheme.secondary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            shadows: [
+              BoxShadow(
+                color: Theme.of(context).colorScheme.shadow.withAlpha(0x25),
+                blurRadius: 2,
+              ),
+            ],
+          ),
+          child: Text(
+            event.name,
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              color: isToday
+                  ? Theme.of(context).colorScheme.onSurfaceVariant
+                  : Theme.of(context).colorScheme.onSecondary,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              height: 1,
+            ),
+          ),
+        ),
       ),
     );
   }
