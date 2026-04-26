42183c10d9cb7e9b73ba1ccade53ce6048bf911a
maria luiza <151561030+maluviieira@users.noreply.github.com>
2026-04-26 11:38:36 +0100

    added home_widget plugin and created widget_service
    Co-authored-by: Francisca Portugal <franpts2@users.noreply.github.com>

---
+++ packages/uni_app/lib/model/services/widget_service.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/foundation.dart';
+import 'package:home_widget/home_widget.dart';
+
+class WidgetService {
+  /// iOS
+  static const iOSWidgetAppGroupId = 'group.uniApp';
+  static const iOSWidgetName = 'ScheduleWidget';
+
+  /// Android
+  static const androidPackagePrefix = 'com.example.foo';
+  static const androidWidgetName =
+      '$androidPackagePrefix.receivers.ScheduleWidgetReceiver';
+
+  /// Called in main.dart
+  static Future<void> initialize() async {
+    await HomeWidget.setAppGroupId(iOSWidgetAppGroupId);
+  }
+
+  /// Save data to Shared Preferences
+  static Future<void> _saveData<T>(String key, T data) async {
+    await HomeWidget.saveWidgetData<T>(key, data);
+  }
+
+  /// Retrieve data from Shared Preferences
+  static Future<T?> _getData<T>(String key) async {
+    return await HomeWidget.getWidgetData<T>(key);
+  }
+
+  /// Request to update widgets on both iOS and Android
+  static Future<void> _updateWidget({
+    String? iOSWidgetName,
+    String? qualifiedAndroidName,
+  }) async {
+    final result = await HomeWidget.updateWidget(
+      iOSName: iOSWidgetName,
+      qualifiedAndroidName: qualifiedAndroidName,
+    );
+    debugPrint(
+      '[WidgetService.updateWidget] iOSWidgetName: $iOSWidgetName, qualifiedAndroidName: $qualifiedAndroidName, result: $result',
+    );
+  }
+}
