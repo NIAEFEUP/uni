fdc4b29d5bf51702744b5083efbd5ff493ad4942
Pedro Lunet <pedrolunet@gmail.com>
2026-04-30 11:29:19 +0100

    fixed provider usage

--- packages/uni_app/lib/model/providers/riverpod/professor_info_provider.dart
+++ packages/uni_app/lib/model/providers/riverpod/professor_info_provider.dart
@@ -3,19 +3,20 @@ import 'package:uni/controller/fetchers/professor_info_fetcher.dart';
 import 'package:uni/model/entities/course_units/sheet.dart';
 import 'package:uni/model/providers/riverpod/session_provider.dart';
 
-final professorInfoProvider = FutureProvider.family<Professor, Professor>((
-  ref,
-  professor,
-) async {
-  final session = await ref.read(sessionProvider.future);
-  final baseUrls = session != null
-      ? List<String>.from(
-          session.faculties.map((f) => 'https://sigarra.up.pt/$f/pt/'),
-        )
-      : <String>[];
+final professorInfoProvider = FutureProvider.autoDispose
+    .family<Professor, Professor>((ref, professor) async {
+  final session = await ref.watch(sessionProvider.future);
+  if (session == null) {
+    throw Exception('Session not available');
+  }
+
+  final baseUrls = List<String>.from(
+    session.faculties.map((f) => 'https://sigarra.up.pt/$f/pt/'),
+  );
+
   return ProfessorInfoFetcher().fetchProfessorInfo(
     professor,
-    session!,
+    session,
     baseUrls,
   );
 });
