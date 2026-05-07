8dbf9a4be60ee37dd76a8089cc4ad76ead113a54
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-05-07 21:31:36 +0100

    Added widget previews

--- packages/uni_app/android/app/src/main/kotlin/pt/up/fe/ni/MainActivity.kt
+++ packages/uni_app/android/app/src/main/kotlin/pt/up/fe/ni/MainActivity.kt
@@ -4,6 +4,12 @@ import io.flutter.embedding.android.FlutterActivity
 import android.os.Build
 import android.os.Bundle
 import androidx.core.view.WindowCompat
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProviderInfo
+import android.content.ComponentName
+import android.widget.RemoteViews
+import pt.up.fe.ni.uni.receivers.ScheduleWidgetReceiver
+import pt.up.fe.ni.uni.receivers.ScheduleWidgetWideReceiver
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +20,26 @@ class MainActivity : FlutterActivity() {
         }
 
         super.onCreate(savedInstanceState)
+        setWidgetPreviews()
+    }
+
+    private fun setWidgetPreviews() {
+        if (Build.VERSION.SDK_INT >= 35) { // Android 15
+            val appWidgetManager = AppWidgetManager.getInstance(this)
+
+            // Small Widget Preview
+            appWidgetManager.setWidgetPreview(
+                ComponentName(this, ScheduleWidgetReceiver::class.java),
+                AppWidgetProviderInfo.WIDGET_CATEGORY_HOME_SCREEN,
+                RemoteViews(packageName, R.layout.schedule_widget_preview)
+            )
+
+            // Wide Widget Preview
+            appWidgetManager.setWidgetPreview(
+                ComponentName(this, ScheduleWidgetWideReceiver::class.java),
+                AppWidgetProviderInfo.WIDGET_CATEGORY_HOME_SCREEN,
+                RemoteViews(packageName, R.layout.schedule_widget_wide_preview)
+            )
+        }
     }
 }
