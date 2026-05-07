1f80d8c56a5120f28cdee2c078f023e05c3e02e1
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-05-07 17:45:19 +0100

    Updated common code to match ios'

--- packages/uni_app/lib/model/services/widget_service.dart
+++ packages/uni_app/lib/model/services/widget_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:home_widget/home_widget.dart';
-import 'package:uni/model/entities/lecture.dart';
 
 class WidgetService {
   /// iOS
@@ -18,21 +17,6 @@ class WidgetService {
   /// Called in main.dart
   static Future<void> initialize() async {
     await HomeWidget.setAppGroupId(iOSWidgetAppGroupId);
-  }
-
-  /// Update the schedule widget with the next lecture
-  static Future<void> updateScheduleWidget(List<dynamic> scheduleData) async {
-    final jsonData = jsonEncode(scheduleData);
-    await _saveData('schedule_data', jsonData);
-
-    await _updateWidget(
-      iOSWidgetName: iOSWidgetName,
-      qualifiedAndroidName: androidWidgetName,
-    );
-
-    await _updateWidget(
-      qualifiedAndroidName: androidWidgetWideName,
-    );
   }
 
   /// Save data to Shared Preferences
@@ -58,12 +42,18 @@ class WidgetService {
       '[WidgetService.updateWidget] iOSWidgetName: $iOSWidgetName, qualifiedAndroidName: $qualifiedAndroidName, result: $result',
     );
   }
-}
 
-extension on Iterable<Lecture> {
-  List<Lecture> sortedBy(Comparable Function(Lecture l) selector) {
-    final list = toList();
-    list.sort((a, b) => selector(a).compareTo(selector(b)));
-    return list;
+  static Future<void> updateScheduleWidget(List<dynamic> scheduleData) async {
+    final jsonData = jsonEncode(scheduleData);
+    await HomeWidget.saveWidgetData('schedule_data', jsonData);
+
+    await HomeWidget.updateWidget(
+      iOSName: 'ScheduleWidget',
+      qualifiedAndroidName: androidWidgetName
+    );
+
+    await HomeWidget.updateWidget(
+      qualifiedAndroidName: androidWidgetWideName
+    );
   }
 }
