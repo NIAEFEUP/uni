31109508bf2bdda023e57f0479e5222391998235
João Lopes <158082419+joaolopes15@users.noreply.github.com>
2026-08-18 19:19:13 +0100

    implemented skeletonizer in restaurant card shimmer

--- packages/uni_app/lib/view/home/widgets/restaurants/restaurants_card_shimmer.dart
+++ packages/uni_app/lib/view/home/widgets/restaurants/restaurants_card_shimmer.dart
@@ -1,31 +1,43 @@
 import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
-import 'package:uni_ui/common/generic_squircle.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+import 'package:uni_ui/cards/restaurant_card.dart';
+import 'package:uni_ui/cards/widgets/restaurant_menu_item.dart';
 
 class ShimmerRestaurantsHomeCard extends StatelessWidget {
   const ShimmerRestaurantsHomeCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ExpandablePageView.builder(
-      controller: PageController(viewportFraction: 0.9),
-      itemCount: 6,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(right: 8),
-          child: Shimmer.fromColors(
-            baseColor: Theme.of(context).disabledColor.withAlpha(0x7f),
-            highlightColor: Theme.of(context).disabledColor,
-            child: GenericSquircle(
-              child: Container(
-                height: 180,
-                decoration: const BoxDecoration(color: Colors.white),
+    return Skeletonizer(
+      effect: ShimmerEffect(
+        baseColor: Theme.of(context).colorScheme.onSecondaryFixed,
+        highlightColor: Theme.of(context).colorScheme.onSecondary,
+        duration: const Duration(seconds: 3),
+      ),
+      child: ExpandablePageView.builder(
+        controller: PageController(viewportFraction: 0.9),
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: RestaurantCard(
+              name: BoneMock.name,
+              icon: const Icon(Icons.restaurant),
+              isFavorite: false,
+              onFavoriteToggle: () {},
+              showFavoriteButton: false,
+              menuItems: List.generate(
+                2,
+                (idx) => RestaurantMenuItem(
+                  name: BoneMock.title,
+                  icon: const Icon(Icons.restaurant_menu),
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
