bf807cc3d228158c387a10a76c933eaca75304c0
Gonçalo Pinto <goncalo.luis.pinto@gmail.com>
2026-09-15 10:07:36 +0100

    feat: added android deploy workflow

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,4 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.androidApplication)
@@ -20,6 +22,9 @@ dependencies {
     debugImplementation(libs.compose.uiTooling)
 }
 
+val appVersion = rootProject.file("version.txt").readText().trim()
+val appVersionCode = (project.findProperty("androidVersionCode") as String?)?.toIntOrNull() ?: 1
+
 android {
     namespace = "pt.up.fe.ni.uni"
     compileSdk =
@@ -37,17 +42,31 @@ android {
             libs.versions.android.targetSdk
                 .get()
                 .toInt()
-        versionCode = 530
-        versionName = "3.0"
+        versionCode = appVersionCode
+        versionName = "$appVersion-native.$appVersionCode"
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    signingConfigs {
+        create("release") {
+            val keystorePropertiesFile = rootProject.file("androidApp/key.properties")
+            if (keystorePropertiesFile.exists()) {
+                val keystoreProperties = Properties()
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
